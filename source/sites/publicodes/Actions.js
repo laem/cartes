ae0392f4ef369ab30b6d37d8fd258dcb0a862334
Mael <mael.thomas@data.gouv.fr>
2020-09-14 14:45:51 +0200

    1ère action chiffrée par une règle publicodes

--- source/sites/publicodes/Actions.js
+++ source/sites/publicodes/Actions.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router'
 import emoji from 'react-easy-emoji'
 import tinygradient from 'tinygradient'
@@ -11,6 +11,12 @@ import BallonGES from './images/ballonGES.svg'
 import SessionBar from 'Components/SessionBar'
 import { Link } from 'react-router-dom'
 import { humanWeight } from './HumanWeight'
+import {
+	flatRulesSelector,
+	analysisWithDefaultsSelector,
+} from 'Selectors/analyseSelectors'
+import { useDispatch, useSelector } from 'react-redux'
+import { setSimulationConfig } from 'Actions/actions'
 
 const gradient = tinygradient(['#0000ff', '#ff0000']),
 	colors = gradient.rgb(20)
@@ -23,13 +29,24 @@ export default ({}) => {
 		from: { value: 0 },
 	})
 
-	return <AnimatedDiv value={value} score={score} />
+	const config = {
+		objectifs: ["arrêter l'avion"],
+	}
+	const analysis = useSelector(analysisWithDefaultsSelector)
+	const configSet = useSelector((state) => state.simulation?.config)
+
+	const dispatch = useDispatch()
+	useEffect(() => dispatch(setSimulationConfig(config)), [])
+	if (!configSet) return null
+	console.log(analysis)
+
+	return <AnimatedDiv value={value} score={score} analysis={analysis} />
 }
 
-const AnimatedDiv = animated(({ score, value }) => {
+const AnimatedDiv = animated(({ analysis, score, value }) => {
 	return (
 		<div css="padding: 0 .3rem 1rem; max-width: 600px; margin: 0 auto;">
-			<h1 css="margin: 0;font-size: 160%">Mes actions</h1>
+			<h1 css="margin: 0;font-size: 160%">Comment réduire mon empreinte ?</h1>
 			<Action
 				color1={'#e58e26'}
 				color2={'#d0b105'}
@@ -37,6 +54,14 @@ const AnimatedDiv = animated(({ score, value }) => {
 				titre="Diviser ma consommation de viande par 4"
 				empreinte={'-1 tonne'}
 				nom={'viande'}
+			/>
+			<Action
+				color1={'#ff0000'}
+				color2={'#ff1166'}
+				icônes="🍽🍖"
+				titre="Arrêter l'avion"
+				empreinte={(analysis.targets[0].nodeValue / 1000).toFixed(1) + ' tonne'}
+				nom={'avion'}
 			/>
 			<Action
 				color1={'#04a4ac'}
