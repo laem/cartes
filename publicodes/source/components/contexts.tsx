fd2e7158f924d48d74f6160de04dad946bf2083e
Mael <mael.thomas@data.gouv.fr>
2020-11-23 09:37:25 +0000

    Ce dépot n'intervient plus sur publicodes
    Si besoin il y a de fonctionnalités en avance par rapport à betagouv/mon-entreprise qui est l'hôte de publicodes, alors nous ferons un fork destiné à cela.

--- publicodes/source/components/contexts.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react'
-import Engine from '..'
-
-export const BasepathContext = createContext<string>('/documentation')
-export const EngineContext = createContext<Engine<string> | null>(null)
