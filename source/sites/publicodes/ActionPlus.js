5c080d3bc7f617decf4566ede417960c96e5b25b
Mael <mael.thomas@data.gouv.fr>
2020-11-30 17:11:18 +0000

    Bouton de retour à l'action

--- source/sites/publicodes/ActionPlus.js
+++ source/sites/publicodes/ActionPlus.js
@@ -4,6 +4,8 @@ import { useParams } from 'react-router'
 import { flatRulesSelector } from 'Selectors/analyseSelectors'
 import { useSelector } from 'react-redux'
 import { findRuleByDottedName, decodeRuleName } from 'Engine/rules'
+import { Link } from 'react-router-dom'
+import emoji from 'react-easy-emoji'
 
 export default () => {
 	const { encodedName } = useParams()
@@ -16,6 +18,11 @@ export default () => {
 
 	return (
 		<div css="padding: 0 .3rem 1rem; max-width: 600px; margin: 1rem auto;">
+			<Link to={'/actions/' + encodedName}>
+				<button className="ui__ button simple small ">
+					{emoji('◀')} Retour
+				</button>
+			</Link>
 			<div css="margin: 1.6rem 0">
 				<Markdown
 					source={rule.plus || "Cette fiche détaillée n'existe pas encore"}
