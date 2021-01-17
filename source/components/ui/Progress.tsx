4ef7734e3430ee17bcf9ba18a976928d87487b71
Mael <mael.thomas@data.gouv.fr>
2021-01-17 18:32:58 +0100

    Gestion de la couleur

--- source/components/ui/Progress.tsx
+++ source/components/ui/Progress.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { useEngine } from '../utils/EngineContext'
 import './Progress.css'
+import tinygradient from 'tinygradient'
 
 type ProgressProps = {
 	progress: number
@@ -7,14 +9,37 @@ type ProgressProps = {
 	className: string
 }
 
+const gradient = tinygradient([
+		'#78e08f',
+		'#e1d738',
+		'#f6b93b',
+		'#b71540',
+		'#000000',
+	]),
+	colors = gradient.rgb(20),
+	incompressible = 1112,
+	durable = 2000,
+	limit = durable + incompressible
+
+const getBackgroundColor = (score) =>
+	colors[
+		Math.round(score < 200 ? 0 : score > limit ? 19 : (score / limit) * 20)
+	]
 export default function Progress({
 	progress,
 	style,
-	className
+	className,
 }: ProgressProps) {
+	const engine = useEngine()
+	const { nodeValue } = engine.evaluate('bilan')
+	const color = getBackgroundColor(nodeValue)
 	return (
 		<div className={'progress__container ' + className} style={style}>
-			<div className="progress__bar" style={{ width: `${progress * 100}%` }} />
+			{nodeValue}
+			<div
+				className="progress__bar"
+				style={{ width: `${progress * 100}%`, background: color }}
+			/>
 		</div>
 	)
 }
