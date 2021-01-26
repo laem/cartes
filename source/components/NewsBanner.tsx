6fb096dd1ffc311252367a3ad7ee80818fe44727
Mael <mael.thomas@data.gouv.fr>
2021-01-26 14:42:50 +0100

    :newspaper: Affichage des notes de version sur le site

---
+++ source/components/NewsBanner.tsx
@@ -0,0 +1,40 @@
+import { useLocalStorage, writeStorage } from '@rehooks/local-storage'
+import { SitePathsContext } from 'Components/utils/SitePathsContext'
+import { useContext } from 'react'
+import emoji from 'react-easy-emoji'
+import { Link } from 'react-router-dom'
+import lastRelease from '../data/last-release.json'
+
+const localStorageKey = 'last-viewed-release'
+
+export const hideNewsBanner = () =>
+	writeStorage(localStorageKey, lastRelease.name)
+
+export const determinant = (word: string) =>
+	/^[aeiouy]/i.exec(word) ? 'd’' : 'de '
+
+export default function NewsBanner() {
+	const [lastViewedRelease] = useLocalStorage(localStorageKey)
+	const sitePaths = useContext(SitePathsContext)
+
+	// We only want to show the banner to returning visitors, so we initiate the
+	// local storage value with the last release.
+	if (lastViewedRelease === undefined) {
+		hideNewsBanner()
+		return null
+	}
+
+	const showBanner = lastViewedRelease !== lastRelease.name
+
+	return showBanner ? (
+		<div className="ui__ banner news">
+			<span>
+				{emoji('✨')} Découvrez les nouveautés {determinant(lastRelease.name)}
+				<Link to={'/nouveautés'}>{lastRelease.name.toLowerCase()}</Link>
+			</span>
+			<span onClick={hideNewsBanner} className="ui__ close-button">
+				&times;
+			</span>
+		</div>
+	) : null
+}
