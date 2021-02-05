1e6b7f847ffba5a8df011f3e028b5ef2fb134f3d
Mael <mael.thomas@data.gouv.fr>
2021-02-05 17:06:25 +0100

    Meilleurs textes GameOver

--- source/sites/publicodes/GameOver.js
+++ source/sites/publicodes/GameOver.js
@@ -11,6 +11,9 @@ export default () => {
 				justify-content: space-around;
 				text-align: center;
 				height: 70%;
+				h1 {
+					font-size: 250%;
+				}
 			`}
 		>
 			<Steps />
@@ -28,12 +31,14 @@ const Steps = () => {
 					<strong>Vous n'êtes pas écolo.</strong>
 				</p>
 				<p>
-					Pourquoi ? Après 5 questions sur 20, nous avons déjà calculé que votre
-					train de vie nous emène vers une planète anormalement réchauffée.
+					Pourquoi ? Après 5 questions sur 20, votre train de vie nous emène
+					déjà vers une planète anormalement réchauffée.
 				</p>
+				[caler l'avancemement visuel ici]
 				<p>
-					On ne vous avait jamais dit que c'était si compliqué ? Oui, ce genre
-					de choses on préfère ne pas en parler.
+					On ne vous avait jamais dit que c'était si compliqué ? Eh oui, face à
+					la falaise on enfile les œillères des petits gestes qui nous allègent
+					la conscience et de la technologie qui nous rassure.
 				</p>
 				<button className="ui__ button plain" onClick={() => setStep(2)}>
 					Recommencer
@@ -49,8 +54,8 @@ const Steps = () => {
 					vies pour réparer nos erreurs.{' '}
 				</p>
 				<p>
-					Chaque année, on pourrit la planète et notre futur et il n'y aura pas
-					de bouton recommencer.
+					Chaque année, on pourrit la planète et notre futur mais il n'y aura{' '}
+					<strong>pas de bouton recommencer</strong>.
 				</p>
 				<button className="ui__ button plain" onClick={() => setStep(3)}>
 					Que faire ?
@@ -60,8 +65,8 @@ const Steps = () => {
 	if (step === 3)
 		return (
 			<>
-				<h1>C'est déprimant, hein ?</h1>
-				<p>Oui, il y a de quoi désespérer.</p>
+				<h1>Changer puis en parler</h1>
+				<p>Il y a de quoi désespérer.</p>
 				<p>
 					Quand on est tout seul, on déprime. Quand on est beaucoup, on agit.
 					C'est de notre planète, notre futur, notre paix, notre bonheur qu'il
@@ -69,7 +74,7 @@ const Steps = () => {
 				</p>
 				<p>
 					Vous avez des amis, de la famille, l'internet ? Partagez-leur ce test
-					⬇️, on n'est tous dans la même merde.
+					⬇️, on est tous dans la même <strong>merde</strong>.
 				</p>
 				<button className="ui__ button plain" onClick={() => setStep(4)}>
 					La bonne nouvelle...
@@ -80,11 +85,12 @@ const Steps = () => {
 		return (
 			<>
 				<h1>Changer, maintenant</h1>
-				<p>Demain vous pourrez peut-etre ne pas prendre votre voiture.</p>
+				<p>Demain vous pourrez ne pas prendre votre voiture.</p>
+				<p>Dans deux semaines acheter un vélo. </p>
 				<p>
-					Dans deux semaines acheter un vélo. L'été prochain vous pouvez choisir
-					d'autres destinations de vacances en train.{' '}
+					L'été prochain choisir d'autres destinations de vacances en train.{' '}
 				</p>
+
 				<p>L'année prochaine déménager, changer de boulot.</p>
 			</>
 		)
