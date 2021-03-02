0850bab45fa6dad9c55154c18330f97d6574cc1d
Mael <mael.thomas@data.gouv.fr>
2021-03-02 17:52:33 +0100

    :art: Affichage des décomptes d'action

--- source/sites/publicodes/Actions.js
+++ source/sites/publicodes/Actions.js
@@ -386,12 +386,12 @@ const CategoryFilter = ({ categories, selected, countByCategory }) => {
 				justify-content: center;
 				li {
 					padding: 0.1rem 0rem;
-					margin: 0.1rem 0.2rem;
+					margin: 0.15rem 0.2rem;
 					border-radius: 0.2rem;
 				}
 				li button {
 					color: white;
-					font-weight: bold;
+					font-weight: 500;
 				}
 			`}
 		>
@@ -402,12 +402,24 @@ const CategoryFilter = ({ categories, selected, countByCategory }) => {
 						${selected === category.dottedName
 							? 'border: 3px solid var(--color)'
 							: ''}
+						${!countByCategory[category.dottedName] ? 'background: #ccc' : ''}
 					`}
 				>
 					<Link to={'/actions/catégorie/' + category.dottedName}>
 						<button>
-							{category.dottedName} ({countByCategory[category.dottedName] || 0}
-							)
+							{category.dottedName}{' '}
+							<span
+								css={`
+									background: white;
+									color: var(--color);
+									border-radius: 1rem;
+									width: 1rem;
+									margin-left: 0.2rem;
+									display: inline-block;
+								`}
+							>
+								{countByCategory[category.dottedName] || 0}
+							</span>
 						</button>
 					</Link>
 				</li>
