502ca4232a33713f0d77a12eb0a17381bf8a4a4c
Mael <mael.thomas@data.gouv.fr>
2021-03-16 12:46:52 +0100

    :art: Plus petit logo pour l'app

--- source/sites/publicodes/App.tsx
+++ source/sites/publicodes/App.tsx
@@ -1,6 +1,9 @@
 import Route404 from 'Components/Route404'
 import 'Components/ui/index.css'
-import React, { Suspense } from 'react'
+import News from 'Pages/News'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { Redirect, useLocation } from 'react-router'
 import { Link, Route, Switch } from 'react-router-dom'
 import Provider from '../../Provider'
 import {
@@ -13,16 +16,13 @@ import Actions from './Actions'
 import Contribution from './Contribution'
 import Fin from './Fin'
 import Landing from './Landing'
-import Logo from './Logo'
+import Logo, { InlineLogo } from './Logo'
 import Documentation from './pages/Documentation'
+import Personas from './Personas.tsx'
 import Privacy from './Privacy'
 import Simulateur from './Simulateur'
-import BandeauContribuer from './VersionBeta'
-import { useTranslation } from 'react-i18next'
 import sitePaths from './sitePaths'
-import News from 'Pages/News'
-import { Redirect } from 'react-router'
-import Personas from './Personas.tsx'
+import BandeauContribuer from './VersionBeta'
 
 let tracker = devTracker
 if (process.env.NODE_ENV === 'production') {
@@ -65,50 +65,55 @@ export default function Root({}) {
 	)
 }
 
-const Router = ({}) => (
-	<>
-		<div className="ui__ container">
-			<nav css="display: flex; justify-content: center; margin: .6rem auto">
-				<Link
-					to="/"
-					css={`
-						display: flex;
-						align-items: center;
-						text-decoration: none;
-						font-size: 170%;
-						margin-bottom: 0.4rem;
-					`}
-				>
-					<Logo />
-				</Link>
-			</nav>
-			<Switch>
-				<Route exact path="/" component={Landing} />
-				{/* Removes trailing slashes */}
-				<Route
-					path={'/:url*(/+)'}
-					exact
-					strict
-					render={({ location }) => (
-						<Redirect to={location.pathname.replace(/\/+$/, location.search)} />
-					)}
-				/>
+const Router = ({}) => {
+	const location = useLocation()
+	return (
+		<>
+			<div className="ui__ container">
+				<nav css="display: flex; justify-content: center; margin: .6rem auto">
+					<Link
+						to="/"
+						css={`
+							display: flex;
+							align-items: center;
+							text-decoration: none;
+							font-size: 170%;
+							margin-bottom: 0.4rem;
+						`}
+					>
+						{location.pathname === '/' ? <Logo /> : <InlineLogo />}
+					</Link>
+				</nav>
+				<Switch>
+					<Route exact path="/" component={Landing} />
+					{/* Removes trailing slashes */}
+					<Route
+						path={'/:url*(/+)'}
+						exact
+						strict
+						render={({ location }) => (
+							<Redirect
+								to={location.pathname.replace(/\/+$/, location.search)}
+							/>
+						)}
+					/>
 
-				<Route path="/documentation" component={Documentation} />
-				<Route path="/simulateur/:name+" component={Simulateur} />
-				{/* Lien de compatibilité, à retirer par exemple mi-juillet 2020*/}
-				<Route path="/fin/:score" component={Fin} />
-				<Route path="/fin" component={Fin} />
-				<Route path="/personas" component={Personas} />
-				<Route path="/actions" component={Actions} />
-				<Route path="/contribuer/:input?" component={Contribution} />
-				<Route path="/à-propos" component={About} />
-				<Route path="/vie-privée" component={Privacy} />
-				<Route path="/nouveautés" component={News} />
+					<Route path="/documentation" component={Documentation} />
+					<Route path="/simulateur/:name+" component={Simulateur} />
+					{/* Lien de compatibilité, à retirer par exemple mi-juillet 2020*/}
+					<Route path="/fin/:score" component={Fin} />
+					<Route path="/fin" component={Fin} />
+					<Route path="/personas" component={Personas} />
+					<Route path="/actions" component={Actions} />
+					<Route path="/contribuer/:input?" component={Contribution} />
+					<Route path="/à-propos" component={About} />
+					<Route path="/vie-privée" component={Privacy} />
+					<Route path="/nouveautés" component={News} />
 
-				<Route component={Route404} />
-			</Switch>
-		</div>
-		<BandeauContribuer />
-	</>
-)
+					<Route component={Route404} />
+				</Switch>
+			</div>
+			<BandeauContribuer />
+		</>
+	)
+}
