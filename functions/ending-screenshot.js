9eb48dcd54bbd4b67c5296999c1bd951e4190051
Mael <mael.thomas@data.gouv.fr>
2021-03-16 14:17:18 +0100

    Ebauche de fonction de capture d'écran de fin

---
+++ functions/ending-screenshot.js
@@ -0,0 +1,28 @@
+const chromium = require('chrome-aws-lambda')
+
+exports.handler = async (event, context) => {
+	const pageToScreenshot = JSON.parse(event.body).pageToScreenshot
+
+	const browser = await chromium.puppeteer.launch({
+		executablePath: await chromium.executablePath,
+		args: chromium.args,
+		defaultViewport: chromium.defaultViewport,
+		headless: chromium.headless,
+	})
+
+	const page = await browser.newPage()
+
+	await page.goto(pageToScreenshot)
+
+	const screenshot = await page.screenshot({ encoding: 'binary' })
+
+	await browser.close()
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			message: `Complete screenshot of ${pageToScreenshot}`,
+			buffer: screenshot,
+		}),
+	}
+}
