2730958e447326080c655605515f4ccedfb9ead4
Mael <mael.thomas@data.gouv.fr>
2021-03-23 20:17:30 +0100

    Début de gestion des utilisateurs

--- source/sites/publicodes/Conference.tsx
+++ source/sites/publicodes/Conference.tsx
@@ -1,22 +1,42 @@
-import * as Y from 'yjs'
-import { WebrtcProvider } from 'y-webrtc'
 import { useEffect, useState } from 'react'
+import emoji from 'react-easy-emoji'
 import { useParams } from 'react-router'
 import { Link } from 'react-router-dom'
-import emoji from 'react-easy-emoji'
+import { WebrtcProvider } from 'y-webrtc'
+import * as Y from 'yjs'
+import fruits from './fruits.json'
+
+const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max))
+const getRandomColor = () =>
+	'#' + Math.floor(Math.random() * 16777215).toString(16)
+
 export default () => {
 	const [elements, setElements] = useState([])
+	const [users, setUsers] = useState([])
 	const [newRoom, setNewRoom] = useState(null)
 	const { room } = useParams()
+	console.log(users)
 
 	useEffect(() => {
 		if (!room) return null
 		const ydoc = new Y.Doc()
-		const id = ydoc.clientID
-
-		console.log(id)
 		// clients connected to the same room-name share document updates
 		const provider = new WebrtcProvider(room, ydoc, {})
+		const awareness = provider.awareness
+
+		// You can observe when a any user updated their awareness information
+		awareness.on('change', (changes) => {
+			// Whenever somebody updates their awareness information,
+			// we log all awareness information from all users.
+			setUsers(Array.from(awareness.getStates().values()))
+		})
+
+		awareness.setLocalState({
+			// Define a print name that should be displayed
+			name: fruits[getRandomInt(fruits.length)],
+			// Define a color that should be associated to the user:
+			color: getRandomColor(), // should be a hex color
+		})
 		const yarray = ydoc.get('simulations', Y.Array)
 		yarray.observe((event) => {
 			setElements(yarray.toJSON())
@@ -33,8 +53,29 @@ export default () => {
 						collègues, etc.
 					</p>
 					<p css="color: #78b159; font-weight: bold">
-						{emoji('🟢')} {Math.round(Math.random() * 10)} clients connectés
+						{emoji('🟢')} {users.length} clients connectés
 					</p>
+					<ul
+						css={`
+							display: flex;
+							list-style-type: none;
+							li {
+								margin: 0.6rem;
+							}
+						`}
+					>
+						{users.map((u) => (
+							<li
+								key={u.name}
+								css={`
+									color: ${u.color};
+								`}
+							>
+								{emoji('👤 ')}
+								{u.name}
+							</li>
+						))}
+					</ul>
 				</div>
 			)}
 			{!room && (
