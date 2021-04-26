cefa687a31a2a7747f1633ee448c1e5017e3cb1f
Mael <mael.thomas@data.gouv.fr>
2021-04-26 10:55:12 +0200

    Pas de graphique si pas de données encore dans la conf

--- source/sites/publicodes/conference/Stats.tsx
+++ source/sites/publicodes/conference/Stats.tsx
@@ -18,46 +18,47 @@ const testResults = [12, 25, 22, 8, 4, 7, 9, 8, 11, 10]
 export default ({ elements, users, username }) => {
 	const mean = computeMean(elements),
 		humanMean = computeHumanMean(elements)
-	console.log(elements, users, username, mean, humanMean)
 
 	return (
 		<div>
 			<div css="text-align: center">Moyenne du groupe : {humanMean}</div>
 			<div css="text-align: center">Moyenne française : 11 tonnes</div>
-			<div
-				css={`
-					width: 90%;
-					position: relative;
-					margin: 0 auto;
-					border: 2px solid black;
-					height: 2rem;
-					list-style-type: none;
-					li {
-						position: absolute;
-					}
-					li:first-child {
-						left: 5%;
-					}
-					li:last-child {
-						right: 5%;
-					}
-				`}
-			>
-				<li key="legendLeft">{Math.round(Math.min(...elements) / 1000)}</li>
-				{elements.map((el) => (
-					<li
-						key={el}
-						css={`
-							height: 100%;
-							width: 10px;
-							left: ${(el / Math.max(...elements)) * 100 * 0.8}%;
-							background: black;
-							opacity: 0.2;
-						`}
-					></li>
-				))}
-				<li key="legendRight">{Math.round(Math.max(...elements) / 1000)}</li>
-			</div>
+			{!isNaN(mean) && (
+				<div
+					css={`
+						width: 90%;
+						position: relative;
+						margin: 0 auto;
+						border: 2px solid black;
+						height: 2rem;
+						list-style-type: none;
+						li {
+							position: absolute;
+						}
+						li:first-child {
+							left: 5%;
+						}
+						li:last-child {
+							right: 5%;
+						}
+					`}
+				>
+					<li key="legendLeft">{Math.round(Math.min(...elements) / 1000)}</li>
+					{elements.map((el) => (
+						<li
+							key={el}
+							css={`
+								height: 100%;
+								width: 10px;
+								left: ${(el / Math.max(...elements)) * 100 * 0.8}%;
+								background: black;
+								opacity: 0.2;
+							`}
+						></li>
+					))}
+					<li key="legendRight">{Math.round(Math.max(...elements) / 1000)}</li>
+				</div>
+			)}
 		</div>
 	)
 }
