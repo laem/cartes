4a4c4c11c136ece9b6bfe41349374ce1d07b4388
Mael <mael.thomas@data.gouv.fr>
2021-04-27 16:37:59 +0200

    Meilleure ordre pour la visu conférence

--- source/sites/publicodes/conference/Stats.tsx
+++ source/sites/publicodes/conference/Stats.tsx
@@ -41,46 +41,48 @@ export default ({ elements, users, username }) => {
 
 	return (
 		<div>
-			<div css="text-align: center">Moyenne du groupe : {humanMean}</div>
-			<div css="text-align: center">Moyenne française : 11 tonnes</div>
-			<div
-				css={`
-					width: 90%;
-					position: relative;
-					margin: 0 auto;
-					border: 2px solid black;
-					height: 2rem;
-					list-style-type: none;
-					li {
-						position: absolute;
-					}
-					li:first-child {
-						left: 5%;
-					}
-					li:last-child {
-						right: 5%;
-					}
-				`}
-			>
-				<li key="legendLeft">{Math.round(Math.min(...values) / 1000)}</li>
-				{Object.entries(elements).map(([usernameI, { bilan: value }]) => (
-					<li
-						key={usernameI}
-						css={`
-							height: 100%;
-							width: 10px;
-							left: ${(value / Math.max(...values)) * 100 * 0.8}%;
-							background: ${users.find((u) => u.name === usernameI)?.color ||
-							'black'};
-							opacity: 0.5;
-						`}
-					></li>
-				))}
-				<li key="legendRight">{Math.round(Math.max(...values) / 1000)}</li>
-			</div>
-			<div css="margin: .6rem; text-align: center">
+			<div css=" text-align: center">
 				<p>Avancement du groupe</p>
 				<Progress progress={meanProgress} />
+			</div>
+			<div css="margin: 1.6rem 0">
+				<div css="text-align: center">Moyenne du groupe : {humanMean}</div>
+				<div css="text-align: center">Moyenne française : 11 tonnes</div>
+				<div
+					css={`
+						width: 90%;
+						position: relative;
+						margin: 0 auto;
+						border: 2px solid black;
+						height: 2rem;
+						list-style-type: none;
+						li {
+							position: absolute;
+						}
+						li:first-child {
+							left: 5%;
+						}
+						li:last-child {
+							right: 5%;
+						}
+					`}
+				>
+					<li key="legendLeft">{Math.round(Math.min(...values) / 1000)}</li>
+					{Object.entries(elements).map(([usernameI, { bilan: value }]) => (
+						<li
+							key={usernameI}
+							css={`
+								height: 100%;
+								width: 10px;
+								left: ${(value / Math.max(...values)) * 100 * 0.8}%;
+								background: ${users.find((u) => u.name === usernameI)?.color ||
+								'black'};
+								opacity: 0.5;
+							`}
+						></li>
+					))}
+					<li key="legendRight">{Math.round(Math.max(...values) / 1000)}</li>
+				</div>
 			</div>
 			<ul
 				css={`
