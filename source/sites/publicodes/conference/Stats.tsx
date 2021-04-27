d6cc93221548a9c8a5e1172396045f24a85efaf8
Mael <mael.thomas@data.gouv.fr>
2021-04-27 16:36:21 +0200

    Affichage des scores par catégorie

--- source/sites/publicodes/conference/Stats.tsx
+++ source/sites/publicodes/conference/Stats.tsx
@@ -30,8 +30,12 @@ export default ({ elements, users, username }) => {
 	if (isNaN(mean)) return null
 
 	const categories = reduceCategories(
-		Object.values(elements).map((el) => el.byCategory)
-	)
+			Object.values(elements).map((el) => el.byCategory)
+		),
+		maxCategory = Object.values(categories).reduce(
+			(memo, next) => Math.max(memo, ...next),
+			0
+		)
 
 	console.log('CAR', categories)
 
@@ -78,6 +82,52 @@ export default ({ elements, users, username }) => {
 				<p>Avancement du groupe</p>
 				<Progress progress={meanProgress} />
 			</div>
+			<ul
+				css={`
+					li:nth-child(2n) {
+						background: var(--lightestColor);
+					}
+					list-style-type: none;
+					li > span {
+						padding-left: 0.6rem;
+						width: 30%;
+						display: inline-block;
+						border-right: 1px solid var(--lightColor);
+					}
+
+					ul {
+						list-style-type: none;
+						display: inline-block;
+						position: relative;
+						width: 70%;
+					}
+					ul li {
+						position: absolute;
+						width: 8px;
+						height: 8px;
+						display: inline-block;
+						background: black;
+						border-radius: 1rem;
+						opacity: 0.1;
+					}
+				`}
+			>
+				{Object.entries(categories).map(([name, values]) => (
+					<li key={name}>
+						<span>{name}</span>
+						<ul>
+							{values.map((value) => (
+								<li
+									key={value}
+									css={`
+										left: ${(value / maxCategory) * 100}%;
+									`}
+								></li>
+							))}
+						</ul>
+					</li>
+				))}
+			</ul>
 		</div>
 	)
 }
