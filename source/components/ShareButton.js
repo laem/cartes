79ecc39f50182819d191a052741679102621a552
Mael <mael.thomas@data.gouv.fr>
2021-05-04 19:03:37 +0200

    Fond de couleur sous le bouton partager

--- source/components/ShareButton.js
+++ source/components/ShareButton.js
@@ -7,7 +7,6 @@ export default (props) =>
 				margin: 0 auto;
 				display: flex;
 				align-items: center;
-				color: ${props.color || 'black'};
 			`}
 			onClick={() =>
 				navigator
@@ -16,7 +15,7 @@ export default (props) =>
 					.catch((error) => console.log('Error sharing', error))
 			}
 		>
-			<Icon color={props.color} />
+			<Icon />
 			{props.label && <span>{props.label}</span>}
 			{/* Created by Barracuda from the Noun Project */}
 		</button>
@@ -52,7 +51,7 @@ const DesktopShareButton = (props) => {
 					}
 				`}
 			>
-				<Icon color={props.color} />
+				<Icon />
 				{props.label && <span>{props.label}</span>}
 			</div>
 			<form css="text-align: center">
@@ -102,28 +101,39 @@ const DesktopShareButton = (props) => {
 	)
 }
 
-const Icon = ({ color = 'black' }) => (
-	<svg version="1.1" x="0px" y="0px" viewBox="0 0 100 100" width="4rem">
-		<g transform="translate(0,-952.36218)">
-			<path
-				css={`
-					text-indent: 0;
-					text-transform: none;
-					direction: ltr;
-					block-progression: tb;
-					baseline-shift: baseline;
-					color: ${color};
-					enable-background: accumulate;
-				`}
-				d="m 67,971.36217 c -5.4991,0 -10,4.50082 -10,10 0,1.123 0.1857,2.20885 0.5312,3.21875 l -17.125,11.125 c -1.8339,-2.03453 -4.4689,-3.34375 -7.4062,-3.34375 -5.4991,0 -10,4.50082 -10,10.00003 0,5.4992 4.5009,10 10,10 2.9494,0 5.6028,-1.2946 7.4375,-3.3437 l 17.0937,11.1249 c -0.3515,1.0177 -0.5312,2.0857 -0.5312,3.2188 0,5.4992 4.5009,10 10,10 5.4991,0 10,-4.5008 10,-10 0,-5.4992 -4.5009,-10 -10,-10 -2.9373,0 -5.5723,1.3092 -7.4062,3.3438 l -17.125,-11.125 c 0.3455,-1.0099 0.5312,-2.0958 0.5312,-3.2188 0,-1.1331 -0.1797,-2.2011 -0.5312,-3.21873 l 17.0937,-11.12505 c 1.8347,2.04912 4.4881,3.34375 7.4375,3.34375 5.4991,0 10,-4.50082 10,-10 0,-5.49918 -4.5009,-10 -10,-10 z m 0,4 c 3.3373,0 6,2.66262 6,6 0,3.33738 -2.6627,6 -6,6 -3.3373,0 -6,-2.66262 -6,-6 0,-3.33738 2.6627,-6 6,-6 z m -34,21 c 3.3373,0 6,2.6626 6,6.00003 0,3.3374 -2.6627,6 -6,6 -3.3373,0 -6,-2.6626 -6,-6 0,-3.33743 2.6627,-6.00003 6,-6.00003 z m 34,21.00003 c 3.3373,0 6,2.6626 6,6 0,3.3374 -2.6627,6 -6,6 -3.3373,0 -6,-2.6626 -6,-6 0,-3.3374 2.6627,-6 6,-6 z"
-				fill={color}
-				fillOpacity="1"
-				stroke="none"
-				marker="none"
-				visibility="visible"
-				display="inline"
-				overflow="visible"
-			/>
-		</g>
-	</svg>
+const Icon = ({}) => (
+	<div
+		css={`
+			background: var(--color);
+			width: 3rem;
+			height: 3rem;
+			border-radius: 2rem;
+			padding: 0.5rem;
+			margin: 0.6rem;
+		`}
+	>
+		<svg version="1.1" x="0px" y="0px" viewBox="0 0 100 100" width="4rem">
+			<g transform="translate(0,-952.36218)">
+				<path
+					css={`
+						text-indent: 0;
+						text-transform: none;
+						direction: ltr;
+						block-progression: tb;
+						baseline-shift: baseline;
+						color: var(--textColor);
+						enable-background: accumulate;
+					`}
+					d="m 67,971.36217 c -5.4991,0 -10,4.50082 -10,10 0,1.123 0.1857,2.20885 0.5312,3.21875 l -17.125,11.125 c -1.8339,-2.03453 -4.4689,-3.34375 -7.4062,-3.34375 -5.4991,0 -10,4.50082 -10,10.00003 0,5.4992 4.5009,10 10,10 2.9494,0 5.6028,-1.2946 7.4375,-3.3437 l 17.0937,11.1249 c -0.3515,1.0177 -0.5312,2.0857 -0.5312,3.2188 0,5.4992 4.5009,10 10,10 5.4991,0 10,-4.5008 10,-10 0,-5.4992 -4.5009,-10 -10,-10 -2.9373,0 -5.5723,1.3092 -7.4062,3.3438 l -17.125,-11.125 c 0.3455,-1.0099 0.5312,-2.0958 0.5312,-3.2188 0,-1.1331 -0.1797,-2.2011 -0.5312,-3.21873 l 17.0937,-11.12505 c 1.8347,2.04912 4.4881,3.34375 7.4375,3.34375 5.4991,0 10,-4.50082 10,-10 0,-5.49918 -4.5009,-10 -10,-10 z m 0,4 c 3.3373,0 6,2.66262 6,6 0,3.33738 -2.6627,6 -6,6 -3.3373,0 -6,-2.66262 -6,-6 0,-3.33738 2.6627,-6 6,-6 z m -34,21 c 3.3373,0 6,2.6626 6,6.00003 0,3.3374 -2.6627,6 -6,6 -3.3373,0 -6,-2.6626 -6,-6 0,-3.33743 2.6627,-6.00003 6,-6.00003 z m 34,21.00003 c 3.3373,0 6,2.6626 6,6 0,3.3374 -2.6627,6 -6,6 -3.3373,0 -6,-2.6626 -6,-6 0,-3.3374 2.6627,-6 6,-6 z"
+					fill="var(--textColor)"
+					fillOpacity="1"
+					stroke="none"
+					marker="none"
+					visibility="visible"
+					display="inline"
+					overflow="visible"
+				/>
+			</g>
+		</svg>
+	</div>
 )
