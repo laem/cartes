c34ffdf608b4bb73c2d055c84a62677895f545d8
Mael <mael.thomas@data.gouv.fr>
2021-06-08 19:42:16 +0200

    Croix pour effacer le nom choisi par défaut

--- source/sites/publicodes/conference/Conference.tsx
+++ source/sites/publicodes/conference/Conference.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import emoji from 'react-easy-emoji'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router'
@@ -110,34 +110,48 @@ export default () => {
 	)
 }
 
-const NamingBlock = ({ newRoom, setNewRoom }) => (
-	<>
-		<label>
-			<form>
-				<input
-					value={newRoom}
-					className="ui__"
-					onChange={(e) => setNewRoom(e.target.value)}
-					css="width: 90% !important"
-				/>{' '}
-			</form>
-		</label>
+const NamingBlock = ({ newRoom, setNewRoom }) => {
+	const inputRef = useRef(null)
+	return (
+		<>
+			<label>
+				<form>
+					<input
+						value={newRoom}
+						className="ui__"
+						onChange={(e) => setNewRoom(e.target.value)}
+						css="width: 90% !important"
+						ref={inputRef}
+					/>
+					<button
+						onClick={(e) => {
+							setNewRoom('')
+							inputRef.current.focus()
+							e.preventDefault()
+						}}
+						title="Effacer le nom actuel"
+					>
+						{emoji('❌️')}
+					</button>
+				</form>
+			</label>
 
-		<button
-			onClick={() => setNewRoom(generateRoomName())}
-			className="ui__ dashed-button"
-		>
-			{emoji('🔃')} Générer un autre nom
-		</button>
+			<button
+				onClick={() => setNewRoom(generateRoomName())}
+				className="ui__ dashed-button"
+			>
+				{emoji('🔃')} Générer un autre nom
+			</button>
 
-		{newRoom && newRoom.length < 10 && (
-			<p>
-				⚠️ Votre nom de salle est court, vous risquez de vous retrouver avec des
-				inconnus...
-			</p>
-		)}
-	</>
-)
+			{newRoom && newRoom.length < 10 && (
+				<p>
+					⚠️ Votre nom de salle est court, vous risquez de vous retrouver avec
+					des inconnus...
+				</p>
+			)}
+		</>
+	)
+}
 
 const UserBlock = ({ users, username, room }) => (
 	<div>
