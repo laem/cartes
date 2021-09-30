ee1154c2ec65114e16304d8a79a666d3359d699c
Mael <mael.thomas@data.gouv.fr>
2021-09-30 14:09:40 +0200

    :sparkles: Nouveau composant emoji encerclé

--- source/sites/publicodes/SubCategoryBar.tsx
+++ source/sites/publicodes/SubCategoryBar.tsx
@@ -1,13 +1,10 @@
 import { motion } from 'framer-motion'
 import { useState } from 'react'
-import emoji from 'react-easy-emoji'
+import CircledEmojis from '../../components/CircledEmojis'
 import { findContrastedTextColor } from '../../components/utils/colors'
-
-const emojiBackground = '#ffffffa6'
 
 export default ({ nodeValue, total, icons, color, title }) => {
 	const [clicked, click] = useState(false)
-	const emojiComponents = emoji(icons || '')
 	const percent = (nodeValue / total) * 100
 	if (percent < 10) return null // will be unreadable
 
@@ -36,69 +33,7 @@ export default ({ nodeValue, total, icons, color, title }) => {
 					{title}
 				</span>
 			) : (
-				<span
-					key={title}
-					css={`
-						position: relative;
-						font-size: 110%;
-						background: ${emojiBackground};
-						border-radius: 2rem;
-						height: 1.5rem;
-						display: inline-block;
-						width: 1.5rem;
-						z-index: 6;
-					`}
-				>
-					<span
-						key="emoji"
-						css={`
-							img {
-								vertical-align: -0.2em !important;
-							}
-						`}
-					>
-						{emojiComponents[0]}
-					</span>
-					{emojiComponents.length > 1 && (
-						<>
-							<span
-								key="background"
-								css={`
-									z-index: -1;
-									font-size: 70%;
-									position: absolute;
-									bottom: 0px;
-									right: 0px;
-									transform: translate(60%, 10%);
-									background: ${emojiBackground};
-									border-radius: 2rem;
-									padding: 0.15rem;
-									height: 1rem;
-									width: 1rem;
-								`}
-							></span>
-							<span
-								key="emoji2"
-								css={`
-									z-index: 7;
-									font-size: 70%;
-									position: absolute;
-									bottom: 0px;
-									right: 0px;
-									transform: translate(60%, 10%);
-									border-radius: 2rem;
-									height: 1rem;
-									width: 1rem;
-									img {
-										vertical-align: 0.1rem !important;
-									}
-								`}
-							>
-								{emojiComponents[1]}
-							</span>
-						</>
-					)}
-				</span>
+				<CircledEmojis emojis={icons} />
 			)}
 		</motion.li>
 	)
