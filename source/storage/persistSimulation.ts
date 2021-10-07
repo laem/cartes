0b6285e427675c8259bc8c003d1a7d9cd3d5f0ac
Mael <mael.thomas@data.gouv.fr>
2021-10-07 16:16:17 +0200

    De gros changements du state imposent une nouvelle version des données

--- source/storage/persistSimulation.ts
+++ source/storage/persistSimulation.ts
@@ -6,7 +6,7 @@ import { debounce } from '../utils'
 import safeLocalStorage from './safeLocalStorage'
 import { deserialize, serialize } from './serializeSimulation'
 
-const VERSION = 1.1
+const VERSION = 2
 
 const LOCAL_STORAGE_KEY = 'ecolab-climat::persisted-simulation::v' + VERSION
 
