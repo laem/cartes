aab8384087306f775834599949aa8b20aac72921
Mael <mael.thomas@data.gouv.fr>
2021-10-12 16:17:46 +0200

    Pas de décalage sur l'accueil car par de menu

--- source/sites/publicodes/App.tsx
+++ source/sites/publicodes/App.tsx
@@ -79,6 +79,7 @@ export default function Root({}) {
 
 const Router = ({}) => {
 	const location = useLocation()
+	const homePage = location.pathname === '/'
 	return (
 		<>
 			<div
@@ -90,7 +91,10 @@ const Router = ({}) => {
 					}
 
 					@media (min-width: 1200px) {
+						${!homePage &&
+						`
 						transform: translateX(-4vw);
+						`}
 					}
 					${sessionBarMargin}
 				`}
@@ -113,7 +117,7 @@ const Router = ({}) => {
 							justify-content: start;
 							border-right: 1px solid #eee;
 						}
-						${location.pathname === '/' && `display: none`}
+						${homePage && `display: none`}
 					`}
 				>
 					<Link
