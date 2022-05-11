6b025a46d42d2a121762237873264393b9fe1c33
Mael <mael.thomas@data.gouv.fr>
2022-05-11 16:52:24 +0200

    Ajout d'une ombre pour l'indicateur de budget

--- source/sites/publicodes/BudgetBar.tsx
+++ source/sites/publicodes/BudgetBar.tsx
@@ -46,6 +46,11 @@ export default ({
 						white-space: normal;
 						line-height: 1rem;
 					}
+					--shadow-color: 210deg 75% 31%;
+					box-shadow: 0.3px 0.5px 0.7px hsl(var(--shadow-color) / 0.36),
+						0.8px 1.6px 2px -0.8px hsl(var(--shadow-color) / 0.36),
+						2.1px 4.1px 5.2px -1.7px hsl(var(--shadow-color) / 0.36),
+						5px 10px 12.6px -2.5px hsl(var(--shadow-color) / 0.36);
 				`}
 			>
 				<div
