ccb805d2489eacb793054f0d072e69c6907dfdac
Mael <mael.thomas@data.gouv.fr>
2022-11-12 14:36:43 +0100

    Première version cool

--- netlify/edge-functions/og-image.tsx
+++ netlify/edge-functions/og-image.tsx
@@ -1,7 +1,6 @@
 /** @jsxImportSource https://esm.sh/react */
 
 import { ImageResponse } from '../../mod.ts'
-
 export default function handler(req) {
 	const { searchParams } = new URL(req.url)
 	const emojis = searchParams.get('emojis'),
@@ -9,13 +8,28 @@ export default function handler(req) {
 
 	return new ImageResponse(
 		(
-			<div>
-				<h1>{title}</h1>
+			<div
+				style={{
+					fontSize: 100,
+					color: 'black',
+					background: '#57bff5',
+					width: '100%',
+					height: '100%',
+					padding: '100px',
+					textAlign: 'center',
+					justifyContent: 'center',
+					alignItems: 'center',
+					display: 'flex',
+					flexDirection: 'column',
+				}}
+			>
+				<h1 style={{ fontSize: 80, margin: '0', padding: 0 }}>{title}</h1>
+				<div style={{ fontSize: 400 }}>{emojis}</div>
 			</div>
 		),
 		{
 			width: 1200,
-			height: 630,
+			height: 660,
 			// Supported options: 'twemoji', 'blobmoji', 'noto', 'openmoji', 'fluent', 'fluentFlat'
 			// Default to 'twemoji'
 			emoji: 'openmoji',
