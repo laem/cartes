aeb3568d4e8e5693541108291a645a212c37a19c
Mael <mael.thomas@data.gouv.fr>
2022-11-12 13:56:13 +0100

    On va faire une fonction simple qui génère des emojis

--- netlify/edge-functions/og-image.tsx
+++ netlify/edge-functions/og-image.tsx
@@ -1,51 +1,33 @@
-// /netlify/edge-functions/og.tsx
+/** @jsxImportSource https://esm.sh/react */
 
-import React from 'https://esm.sh/react@18.2.0'
-import { ImageResponse } from 'https://deno.land/x/og_edge/mod.ts'
-import Emoji from '../../source/components/Emoji.tsx'
+import { ImageResponse } from './mod.ts'
 
-export default function handler(req: Request) {
-	const { searchParams } = new URL(req.url)
-	const username = searchParams.get('username')
-
+export default function handler() {
 	return new ImageResponse(
 		(
 			<div
 				style={{
-					fontSize: 60,
+					fontSize: 100,
 					color: 'black',
-					background: '#f6f6f6',
+					background: 'white',
 					width: '100%',
 					height: '100%',
-					paddingTop: 50,
-					flexDirection: 'column',
+					padding: '50px 200px',
+					textAlign: 'center',
 					justifyContent: 'center',
 					alignItems: 'center',
 					display: 'flex',
 				}}
 			>
-				{/* eslint-disable-next-line @next/next/no-img-element */}
-				<img
-					alt="avatar"
-					width="256"
-					src={`https://github.com/${username || 'netlify'}.png`}
-					style={{
-						borderRadius: 128,
-					}}
-				/>
-				{username ? (
-					<>
-						<p>YOYOgithub.com/{username}</p>
-						<Emoji e="🗽" />
-					</>
-				) : (
-					<p>Mon nozdazdm d'ut /?username=ascorbic</p>
-				)}
+				👋, 🌎
 			</div>
 		),
 		{
 			width: 1200,
 			height: 630,
+			// Supported options: 'twemoji', 'blobmoji', 'noto', 'openmoji', 'fluent', 'fluentFlat'
+			// Default to 'twemoji'
+			emoji: 'twemoji',
 		}
 	)
 }
