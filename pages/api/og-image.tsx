8363f46c36a223874332c3e4bb3f7d54f2c7c0a8
Mael <mael.thomas@data.gouv.fr>
2023-06-28 15:52:40 +0200

    Propriété de config manquante

--- pages/api/og-image.tsx
+++ pages/api/og-image.tsx
@@ -1,5 +1,9 @@
 import { ImageResponse } from '@vercel/og'
 
+export const config = {
+	runtime: 'edge',
+}
+
 export default function handler(req) {
 	const { searchParams } = new URL(req.url)
 	const emojis = searchParams.get('emojis'),
