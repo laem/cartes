80a968f04bcab71deef618b3964b00c8aafd2949
Mael <mael.thomas@data.gouv.fr>
2023-08-08 11:40:32 +0200

    Ce n'est pas publicodes qui plante.
    Prochaine étape : remplacer RulesProvider par les mécanique de
    chargement de données prévues par Next ?

--- app/montest/[...mavar]/page.tsx
+++ app/montest/[...mavar]/page.tsx
@@ -1,1 +1,8 @@
-export default ({ params: { mavar } }) => mavar
+import Engine from 'publicodes'
+
+export default ({ params: { mavar } }) => {
+	const engine = new Engine({ a: 'b + c', b: 3, c: 26 })
+	const evaluation = engine.evaluate('a')
+
+	return mavar + evaluation.nodeValue
+}
