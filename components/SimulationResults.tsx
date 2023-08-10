1f772e7795fc87779e227f91c2cd8fe09a0ce85b
Mael <mael.thomas@data.gouv.fr>
2023-08-10 11:38:34 +0200

    Hover inutile

--- components/SimulationResults.tsx
+++ components/SimulationResults.tsx
@@ -30,18 +30,9 @@ const SimulationResults = ({
 				color: var(--darkColor);
 				margin: 1rem auto;
 				border-radius: 0.3rem;
-				box-shadow: 0 1px 3px rgba(41, 117, 209, 0.12),
-					0 1px 2px rgba(41, 117, 209, 0.24);
-				transition: box-shadow 0.2s;
-				:hover {
-					opacity: 1 !important;
-					box-shadow: 0px 2px 4px -1px rgba(41, 117, 209, 0.2),
-						0px 4px 5px 0px rgba(41, 117, 209, 0.14),
-						0px 1px 10px 0px rgba(41, 117, 209, 0.12);
-				}
 			`}
 		>
-			<ProgressCircle engine={engine}/>
+			<ProgressCircle engine={engine} />
 			<div
 				id="shareImage"
 				css={`
