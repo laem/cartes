ad81dc4a126dd6fc02556fe4d9dd8f20954762a8
Mael <mael.thomas@data.gouv.fr>
2023-08-23 15:03:17 +0200

    Correction du github contribution link

--- components/documentation/GithubContributionLink.tsx
+++ components/documentation/GithubContributionLink.tsx
@@ -1,4 +1,5 @@
 import { LightButton } from '../UI'
+import { title } from '../utils/publicodesUtils'
 
 // Not integratable yet, see https://github.com/betagouv/publicodes/issues/336
 export const GithubContributionLink = ({ dottedName }) => (
@@ -11,7 +12,7 @@ export const GithubContributionLink = ({ dottedName }) => (
 	>
 		<a
 			href={`https://github.com/search?q=${encodeURIComponent(
-				`repo:datagir/nosgestesclimat "${dottedName}:"`
+				`repo:laem/futureco-data "${title({ dottedName })}:"`
 			)} path:data&type=code`}
 		>
 			<LightButton>✏️ Contribuer</LightButton>
