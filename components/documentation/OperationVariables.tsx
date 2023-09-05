6bc4ad50f0bf949e552eb49ca46eb97994c0c89b
Mael <mael.thomas@data.gouv.fr>
2023-09-05 18:03:27 +0200

    Belle amélioration de l'algo de détection d'expression
    Il peut gérer des expressions plus complexes que 2 membres

--- components/documentation/OperationVariables.tsx
+++ components/documentation/OperationVariables.tsx
@@ -4,28 +4,31 @@ import Link from 'next/link'
 import { title as ruleTitle } from 'Components/utils/publicodesUtils'
 import { VariableList } from './DocumentationStyle'
 
+const binaryOps = ['+', '-', '*', '/', '>', '<', '>=', '<=', '=', '!=']
+
+const findVariables = (parsed) => {
+	if (parsed.variable) return [parsed.variable]
+	const isBinaryOp = binaryOps.find((op) => parsed[op])
+	if (isBinaryOp)
+		return Object.entries(parsed[isBinaryOp]).map(([k, v]) => findVariables(v))
+}
 const findSimpleOperationToLink = (expression, rules, dottedName) => {
 	const parsed = parseExpression(expression)
-	console.log('P', parsed)
-	const operation = parsed.variable
-		? [null, [{ variable: parsed.variable }]] // artificially create the shape of an operation (e.g. *) with a single element
-		: Object.entries(parsed).find(
-				([k, v]) => Array.isArray(v) && v.some((el) => el.variable != null)
-		  )
-	if (!operation) return null
-	const [key, value] = operation
+	console.log('P', JSON.stringify(parsed))
 
+	const variables = findVariables(parsed)
+	if (!variables || !Array.isArray(variables)) return null
 	// HACK when parsed, publicode rules get the "private" attribute, mostly to "false".
 	// But here, we're using disambiguateReference on unparsed rules. Hence we need to set all of them to private: false to avoid throwing errors
 	const publicRules = Object.fromEntries(
 		Object.entries(rules).map(([k, v]) => [k, { ...v, private: false }])
 	)
 
-	const references = value
-		.filter((el) => el.variable != null)
-		.map((el) =>
-			utils.disambiguateReference(publicRules, dottedName, el.variable)
-		)
+	console.log('VAR', variables.flat(Infinity))
+	const references = variables
+		.flat(Infinity)
+		.filter((el) => el != null)
+		.map((el) => utils.disambiguateReference(publicRules, dottedName, el))
 	return references
 }
 
