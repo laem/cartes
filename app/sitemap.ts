e735c416894ba27f5a13aeaeef0534a595a6bb0c
Mael <mael.thomas@data.gouv.fr>
2023-09-05 19:25:00 +0200

    Génération du sitemap en next

---
+++ app/sitemap.ts
@@ -0,0 +1,59 @@
+const lines = [
+	'https://futur.eco',
+	'https://futur.eco/a-propos',
+	//	'https://futur.eco/documentation',
+	'https://futur.eco/scenarios',
+]
+
+const getResults = () =>
+	fetch('https://futureco-data.netlify.app/co2.json')
+		.then((res) => res.json())
+		.then((json) => {
+			const documentationLines = Object.keys(json).map(
+				(dottedName) =>
+					`https://futur.eco/documentation/${encodeRuleName(dottedName)}`
+			)
+			const simulationLines = Object.keys(json)
+				.filter(
+					(dottedName) => json[dottedName] && json[dottedName]['exposé'] != null
+				)
+				.map(
+					(dottedName) =>
+						`https://futur.eco/simulateur/${encodeRuleName(dottedName)}`
+				)
+
+			const results = [...lines, ...simulationLines, ...documentationLines]
+			return results
+		})
+
+/* Unfortunately, we can't yet import this function from engine/rules */
+const encodeRuleName = (name) =>
+	name
+		.replace(/\s\.\s/g, '/')
+		.replace(/-/g, '\u2011') // replace with a insecable tiret to differenciate from space
+		.replace(/\s/g, '-')
+
+export default async function sitemap() {
+	const results = await getResults()
+	return results.map((el) => ({ url: el }))
+	return [
+		{
+			url: 'https://acme.com',
+			lastModified: new Date(),
+			changeFrequency: 'yearly',
+			priority: 1,
+		},
+		{
+			url: 'https://acme.com/about',
+			lastModified: new Date(),
+			changeFrequency: 'monthly',
+			priority: 0.8,
+		},
+		{
+			url: 'https://acme.com/blog',
+			lastModified: new Date(),
+			changeFrequency: 'weekly',
+			priority: 0.5,
+		},
+	]
+}
