89c3c0cff592734452276f032ca7fab8efd802f0
Mael <mael.thomas@data.gouv.fr>
2023-09-12 23:39:33 +0200

    Ligne manquante dans le sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,6 +3,8 @@ const lines = [
 	'https://futur.eco/a-propos',
 	//	'https://futur.eco/documentation',
 	'https://futur.eco/scenarios',
+	'https://futur.eco/national',
+	'https://futur.eco/carburants/prix-a-la-pompe',
 ]
 
 const getResults = () =>
@@ -42,18 +44,6 @@ export default async function sitemap() {
 			lastModified: new Date(),
 			changeFrequency: 'yearly',
 			priority: 1,
-		},
-		{
-			url: 'https://acme.com/about',
-			lastModified: new Date(),
-			changeFrequency: 'monthly',
-			priority: 0.8,
-		},
-		{
-			url: 'https://acme.com/blog',
-			lastModified: new Date(),
-			changeFrequency: 'weekly',
-			priority: 0.5,
 		},
 	]
 }
