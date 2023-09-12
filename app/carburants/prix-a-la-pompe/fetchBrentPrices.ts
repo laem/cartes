4f74a72a4db47db4a4c6b607aa82eb6793bec9b1
Mael <mael.thomas@data.gouv.fr>
2023-09-12 21:17:15 +0200

    Réparation de l'appel pour le prix du brent

--- app/carburants/prix-a-la-pompe/fetchBrentPrices.ts
+++ app/carburants/prix-a-la-pompe/fetchBrentPrices.ts
@@ -1,17 +1,20 @@
-export default () =>
-	fetch(
-		'https://api.eia.gov/series/?api_key=hnH14aRQSrK2Ds4uV9u7uGZt6LvcjWsjKYZpKJG8&series_id=PET.RBRTE.D'
+export default function fetchBrentPrices() {
+	return fetch(
+		'https://api.eia.gov/v2/seriesid/PET.RBRTE.D?api_key=hnH14aRQSrK2Ds4uV9u7uGZt6LvcjWsjKYZpKJG8'
 	)
 		.then((res) => res.json())
 		.then((json) => {
-			const data = json.series[0].data[0]
-			return [parseDate(data[0]), data[1]]
+			const data = json.response.data[0]
+			const result = [parseDate(data.period), data.value]
+			console.log(result)
+			return result
 		})
+}
 
 export function parseDate(str) {
 	var y = str.substr(0, 4),
-		m = str.substr(4, 2) - 1,
-		d = str.substr(6, 2)
+		m = str.substr(5, 2) - 1,
+		d = str.substr(8, 2)
 	var D = new Date(y, m, d)
 	return D.getFullYear() == y && D.getMonth() == m && D.getDate() == d
 		? D
