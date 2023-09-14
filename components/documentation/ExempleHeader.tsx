d7373b4cbe29381847374a03786512401da08b52
Mael <mael.thomas@data.gouv.fr>
2023-09-14 14:36:15 +0200

    L'exemple ne fait plus sauter la page

--- components/documentation/ExempleHeader.tsx
+++ components/documentation/ExempleHeader.tsx
@@ -1,35 +1,44 @@
 'use client'
 
 import { useSelector } from 'react-redux'
+import styled from 'styled-components'
 import FriendlyObjectViewer from '../FriendlyObjectViewer'
 import { Circle } from './DocumentationStyle'
 
-export default () => {
+export default function ExempleHeader() {
 	const exemple = useSelector((state) => state.exemple)
 
-	if (!exemple) return null
+	if (!exemple) return <Wrapper />
 
 	return (
-		<div
-			css={`
-				display: flex;
-				flex-direction: column;
-				align-items: end;
-			`}
-		>
-			<div>
-				<Circle $clicked />
-				{exemple.titre}
-			</div>
-			<div
-				css={`
-					> div {
-						margin: 0.8rem 4rem 1.6rem;
-					}
-				`}
-			>
-				<FriendlyObjectViewer data={exemple.situation} />
-			</div>
-		</div>
+		<Wrapper>
+			<details>
+				<summary>
+					<Circle $clicked />
+					{exemple.titre}
+				</summary>
+				<div
+					css={`
+						> div {
+							margin: 0.8rem 4rem 1.6rem;
+						}
+					`}
+				>
+					<FriendlyObjectViewer data={exemple.situation} />
+				</div>
+			</details>
+		</Wrapper>
 	)
 }
+
+const Wrapper = styled.div`
+	min-height: 2.2rem;
+	details {
+	}
+	summary {
+		display: flex;
+		align-items: center;
+		cursor: pointer;
+		justify-content: end;
+	}
+`
