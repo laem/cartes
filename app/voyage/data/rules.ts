c093380c1815c679eb46cd9b9c58237310ba0949
Mael <mael.thomas@data.gouv.fr>
2023-09-14 16:07:56 +0200

    Séparation des fichiers publicodes : couts fixes

--- app/voyage/data/rules.ts
+++ app/voyage/data/rules.ts
@@ -1,6 +1,7 @@
 import train from './train.yaml'
 import trajet from './trajet.yaml'
 import voiture from './voiture.yaml'
+import fixes from './voiture . coûts fixes.yaml'
 
-const rules = { ...train, ...voiture, ...trajet }
+const rules = { ...train, ...voiture, ...trajet, ...fixes }
 export default rules
