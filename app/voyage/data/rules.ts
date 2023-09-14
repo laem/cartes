3b233313f1795bd64e0843273eed8839c4556e2c
Mael <mael.thomas@data.gouv.fr>
2023-09-14 17:22:54 +0200

    Gros renommage du modèle

--- app/voyage/data/rules.ts
+++ app/voyage/data/rules.ts
@@ -1,7 +1,7 @@
 import train from './train.yaml'
 import trajet from './trajet.yaml'
 import voiture from './voiture.yaml'
-import fixes from './voiture . coûts fixes.yaml'
+import possession from './voiture . coûts de possession.yaml'
 
-const rules = { ...train, ...voiture, ...trajet, ...fixes }
+const rules = { ...train, ...voiture, ...trajet, ...possession }
 export default rules
