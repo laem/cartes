3e61cdbbc207e263093c5f6d7fb0b75bb91a6343
Mael <mael.thomas@data.gouv.fr>
2023-09-14 17:29:50 +0200

    Nouvelle catégorie de coûts : divers vs possession

--- app/voyage/data/rules.ts
+++ app/voyage/data/rules.ts
@@ -2,6 +2,7 @@ import train from './train.yaml'
 import trajet from './trajet.yaml'
 import voiture from './voiture.yaml'
 import possession from './voiture . coûts de possession.yaml'
+import divers from './voiture . coûts divers.yaml'
 
-const rules = { ...train, ...voiture, ...trajet, ...possession }
+const rules = { ...train, ...voiture, ...trajet, ...possession, ...divers }
 export default rules
