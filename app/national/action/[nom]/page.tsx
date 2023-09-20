116382814c967ba2e68a550bcf6e6a9d4c6e54fb
Mael <mael.thomas@data.gouv.fr>
2023-09-20 13:45:43 +0200

    Amélioration action tram

--- app/national/action/[nom]/page.tsx
+++ app/national/action/[nom]/page.tsx
@@ -3,6 +3,8 @@ import { utils } from 'publicodes'
 const { decodeRuleName } = utils
 import { MDXRemote } from 'next-mdx-remote/rsc'
 import Article from '@/components/Article'
+import Link from 'next/link'
+import Emoji from '@/components/Emoji'
 
 const Page = ({ params }) => {
 	const { nom: raw } = params
@@ -16,6 +18,10 @@ const Page = ({ params }) => {
 	if (!rule) return <p>Cette action n'existe pas.</p>
 	return (
 		<main>
+			<Link href="/national">
+				<Emoji e="◀️" />
+				Retour au tableau de bord de la planfication écologique
+			</Link>
 			<h1>{rule.titre}</h1>
 			<Article>
 				<MDXRemote source={rule.notes} />
