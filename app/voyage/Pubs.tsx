8fe82806ea9c36fca48658c747462f1718f0eb49
Mael <mael.thomas@data.gouv.fr>
2023-09-26 18:33:50 +0200

    1ère itération Renault Zoé

--- app/voyage/Pubs.tsx
+++ app/voyage/Pubs.tsx
@@ -7,29 +7,54 @@ export default function Pubs() {
 			css={`
 				position: relative;
 				height: 30rem;
-				> * {
+				> div,
+				> img {
 					position: absolute;
+					top: 0;
+					right: 0;
 				}
 				img {
 					width: auto;
 					height: inherit;
-					top: 0;
-					left: 0;
 				}
-				div {
+				> div {
+					right: 1rem;
+				}
+				p {
+					text-align: right;
 					height: inherit;
 					top: 2rem;
 					right: 5rem;
 					color: black;
-					font-size: 300%;
-					max-width: 20rem;
-					line-height: 3rem;
+					font-size: 260%;
+					max-width: 30rem;
+					line-height: 4rem;
+					small {
+						font-size: 75%;
+					}
+					em {
+						font-size: 150%;
+						font-style: normal;
+					}
 				}
 				height: ;
 			`}
 		>
 			<Image src={zoe} />
-			<div>Pour seulement 88&nbsp;000 € sur 25 ans</div>
+			<div>
+				<p>
+					Nouvelle Renault <strong>ZOE</strong>
+				</p>
+				<p>
+					<small>Accessible dès </small>
+				</p>
+				<p>
+					<em>88&nbsp;000 €</em>
+				</p>
+				<p>
+					<small> sur 25 ans. </small>
+				</p>
+			</div>
 		</div>
 	)
 }
