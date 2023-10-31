a1e6ce2c0e406ed3a847d04f67c486a3ed3d920d
Mael <mael.thomas@data.gouv.fr>
2023-10-31 15:02:32 +0100

    Essai non mis en ligne d'une belle illustration ferry sur l'accueil

--- app/wiki/Wiki.tsx
+++ app/wiki/Wiki.tsx
@@ -1,10 +1,10 @@
 import byCategory from 'Components/categories'
 import Emoji from 'Components/Emoji'
 import Link from 'next/link'
-import { utils } from 'publicodes'
-import Highlighter from 'react-highlight-words'
 import Image from 'next/image'
+import { utils } from 'publicodes'
 import energy from '@/public/energy.svg'
+import ferryIcon from '@/public/ferry-small.png'
 
 const { encodeRuleName } = utils
 
@@ -17,20 +17,12 @@ import {
 } from '@/components/WikiUI'
 import { Card } from '@/components/UI'
 import { title as ruleTitle } from 'Components/utils/publicodesUtils'
+import css from '@/components/css/convertToJs'
 
 export default function Wiki({ rules }) {
 	const exposedRules = Object.entries(rules)
 		.map(([dottedName, v]) => ({ ...v, dottedName }))
 		.filter((rule) => rule?.exposé)
-
-	const withCustomSimulators = [
-		...exposedRules,
-		{
-			titre: 'Prix à la pompe',
-			description:
-				'Décomposition du prix des carburants à la pompe (essence, gazole)',
-		},
-	]
 
 	return (
 		<section>
@@ -74,7 +66,7 @@ const CategoryView = ({ exposedRules, rules }) => {
 		</CategoryList>
 	)
 }
-const RuleList = ({ rules, input }) => (
+const RuleList = ({ rules }) => (
 	<RuleListStyle>
 		{rules.map((rule) => {
 			const dottedName = rule.dottedName
@@ -92,19 +84,19 @@ const RuleList = ({ rules, input }) => (
 				<li key={dottedName || rule.titre}>
 					<Link href={rule.url || '/simulateur/' + encodeRuleName(dottedName)}>
 						<WikiCard $inversedColor={rule.inversedColor}>
-							<Emoji e={icônes} />
-							<h3>
-								{input ? (
-									<Highlighter
-										searchWords={input.split(' ')}
-										autoEscape={true}
-										textToHighlight={title}
-										highlightClassName="highlighted"
-									/>
-								) : (
-									title
-								)}
-							</h3>
+							{false && dottedName && dottedName.includes('ferry') ? (
+								<Image
+									src={ferryIcon}
+									alt={'Un ferry dans une mer rouge'}
+									style={css`
+										width: 3rem;
+										height: auto;
+									`}
+								/>
+							) : (
+								<Emoji e={icônes} />
+							)}
+							<h3>{title}</h3>
 							<CardUnits>
 								{units.map((unit) => {
 									const { text, title } = unitRepresentations[unit]
