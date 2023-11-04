5e8a03c837d2aa66559d7ce9a00013edf1978dae
Mael <mael.thomas@data.gouv.fr>
2023-11-04 14:26:27 +0100

    Ça commence

---
+++ app/voyage/page.tsx
@@ -0,0 +1,32 @@
+import { Metadata } from 'next'
+import Voyage from './Voyage2'
+
+const title = `Voyagez, autrement.`
+const description1 =
+	"Comment voyager sans voiture ? Comment gérer les derniers kilomètres après l'arrivée à la gare ? Comment se déplacer pendant le weekend ? Où louer une voiture ou un vélo ? On vous guide, pour que le voyage sans voiture personnelle soit un plaisir."
+
+export const objectives = ['voyage . trajet voiture . coût trajet par personne']
+
+export async function generateMetadata(
+	{ params, searchParams }: Props,
+	parent?: ResolvingMetadata
+): Promise<Metadata> {
+	const image = `/voyage.png`
+	return {
+		title,
+		description: description1,
+		openGraph: {
+			images: [image],
+			type: 'article',
+			publishedTime: '2023-11-01T00:00:00.000Z',
+		},
+	}
+}
+
+const Page = ({ searchParams }) => (
+	<main>
+		<Voyage />
+	</main>
+)
+
+export default Page
