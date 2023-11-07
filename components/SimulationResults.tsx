f1210b9849fdfe7e3671b5c1e70f7a5a4747a2be
Mael <mael.thomas@data.gouv.fr>
2023-11-07 18:10:40 +0100

    Test image origine

--- components/SimulationResults.tsx
+++ components/SimulationResults.tsx
@@ -39,11 +39,19 @@ const SimulationResults = ({
 			<div
 				id="shareImage"
 				css={`
+					position: relative;
 					display: flex;
 					align-items: center;
 					justify-content: center;
 					flex-wrap: wrap;
 					width: 100%;
+					overflow: hidden;
+					> *:first-child {
+						z-index: 0;
+					}
+					> * {
+						z-index: 1;
+					}
 				`}
 			>
 				<div css="width: 100%; img { font-size: 150%}}">
@@ -52,15 +60,30 @@ const SimulationResults = ({
 				<h1
 					css={`
 						width: 100%;
-						font-size: 120%;
+						font-size: 100%;
 						color: var(--darkerColor);
 						margin: 0;
 						margin-bottom: 1rem;
-						line-height: 2rem;
+						width: 80%;
+						background: white;
+						line-height: 1.4rem;
 					`}
 				>
 					{title}
 				</h1>
+				<img
+					src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Marité.jpg/400px-Marité.jpg"
+					css={`
+						position: absolute;
+						top: 50%;
+						transform: translateY(-50%);
+						opacity: 0.8;
+						left: 0;
+						width: 100%;
+						height: auto;
+						object-fit: cover;
+					`}
+				/>
 			</div>
 			<>
 				<div
