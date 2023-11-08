4d96c7ccd1b5770b61d38c54a5a87c9e8aafb92d
Mael <mael.thomas@data.gouv.fr>
2023-11-08 12:16:20 +0100

    WIP on essaie d'utiliser le composant OG directement en HTML dans l'app

---
+++ components/SimulationVignetteOg.tsx
@@ -0,0 +1,113 @@
+import BeautifulSituation from '@/components/BeautifulSituation'
+import css from '@/components/css/convertToJs'
+import { getSituation } from '@/components/utils/simulationUtils'
+import Publicodes, { formatValue } from 'publicodes'
+const futurecoRules = 'https://futureco-data.netlify.app/co2.json'
+import voitureRules from '@/app/voyage/cout-voiture/data/rules'
+
+export default async function SimulationVignetteOg({ searchParams }) {
+	const params = Object.fromEntries(searchParams)
+	const { dottedName, title, emojis, ...encodedSituation } = params
+
+	const rules = await getRules(dottedName)
+	const engine = new Publicodes(rules)
+
+	const validatedSituation = getSituation(encodedSituation, rules)
+	console.log(title, emojis, validatedSituation)
+	const evaluation = engine
+			.setSituation(validatedSituation)
+			.evaluate(dottedName),
+		rawValue = formatValue(evaluation, { precision: 1 }),
+		valueWithoutUnit = formatValue(evaluation, {
+			precision: 1,
+			displayedUnit: '',
+		}),
+		rawUnit = rawValue.split(valueWithoutUnit)[1],
+		[value, unit] = formatUnit(rawUnit, evaluation.nodeValue, valueWithoutUnit)
+	return (
+		<div
+			style={{
+				display: 'flex',
+				height: '100%',
+				width: '100%',
+				alignItems: 'center',
+				justifyContent: 'center',
+				flexDirection: 'column',
+				backgroundImage: 'linear-gradient(to bottom, #dbf4ff, #fff1f1)',
+				fontSize: 100,
+				letterSpacing: -2,
+				fontWeight: 700,
+				textAlign: 'center',
+				lineHeight: 0.8,
+			}}
+		>
+			{
+				/* 
+				<Piece />
+				*/
+				<div
+					style={css(`
+					 font-size: 160;
+					`)}
+				>
+					{emojis}
+				</div>
+			}
+			<div
+				style={{
+					backgroundImage:
+						'linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 216))',
+					backgroundClip: 'text',
+					'-webkit-background-clip': 'text',
+					color: 'transparent',
+				}}
+			>
+				{title}
+			</div>
+			<div
+				style={css`
+					font-size: 30;
+					display: flex;
+					margin-top: 1rem;
+				`}
+			>
+				<BeautifulSituation {...{ validatedSituation, rules }} />
+			</div>
+			<div
+				style={css`
+					display: flex;
+					align-items: center;
+					margin-top: 1rem;
+				`}
+			>
+				<span>{value}</span>
+				<small
+					style={css`
+						font-size: 60;
+						margin-left: 1rem;
+					`}
+				>
+					{unit}
+				</small>
+			</div>
+		</div>
+	)
+}
+const isVoiture = (dottedName) =>
+	dottedName === 'voyage . trajet voiture . coût trajet par personne'
+const getRules = async (dottedName) => {
+	if (isVoiture(dottedName)) return voitureRules
+
+	const rulesRequest = await fetch(futurecoRules, { mode: 'cors' }),
+		rules = await rulesRequest.json()
+	return rules
+}
+
+const formatUnit = (rawUnit, nodeValue, formattedValue) => {
+	console.log('|' + rawUnit + '|')
+	if (rawUnit === ' kgCO₂e') {
+		const [v, u] = humanWeight(nodeValue)
+		return [v, u + ' de CO2e']
+	}
+	return [formattedValue, rawUnit]
+}
