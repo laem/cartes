a44bb0cc20218ab00d1b22dc09d253b708e30484
Mael <mael.thomas@data.gouv.fr>
2023-11-15 14:29:33 +0100

    Affichage d'une date pas trop dégueu

--- app/voyage/OsmFeature.tsx
+++ app/voyage/OsmFeature.tsx
@@ -1,7 +1,9 @@
 import FriendlyObjectViewer from '@/components/FriendlyObjectViewer'
+import parseOpeningHours from 'opening_hours'
 
 export default function OsmFeature({ data }) {
-	const { name, ...rest } = data.tags
+	const { name, opening_hours, ...rest } = data.tags
+
 	return (
 		<div>
 			<h2
@@ -14,6 +16,7 @@ export default function OsmFeature({ data }) {
 			>
 				{name}
 			</h2>
+			{opening_hours && <OpeningHours opening_hours={opening_hours} />}
 			<div
 				css={`
 					> div {
@@ -26,3 +29,52 @@ export default function OsmFeature({ data }) {
 		</div>
 	)
 }
+
+const OpeningHours = ({ opening_hours }) => {
+	const now = new Date()
+	const oh = new parseOpeningHours(opening_hours),
+		isOpen = oh.getState(),
+		nextChange = oh.getNextChange()
+
+	const formatDate = (date) => {
+		const sameDay = date.getDay() === now.getDay()
+		const weekday = sameDay ? undefined : 'long'
+
+		const result = new Intl.DateTimeFormat('fr-FR', {
+			hour: 'numeric',
+			minute: 'numeric',
+			weekday,
+		}).format(nextChange)
+		return result
+	}
+	return (
+		<div
+			css={`
+				display: flex;
+				align-items: center;
+			`}
+		>
+			<details open={false}>
+				<summary>
+					<OpenIndicator isOpen={isOpen} /> {isOpen ? 'Ouvert' : 'Fermé'}{' '}
+					jusqu'à {formatDate(nextChange)}
+				</summary>
+
+				{opening_hours}
+			</details>
+		</div>
+	)
+}
+
+const OpenIndicator = ({ isOpen }) => (
+	<span
+		css={`
+			display: inline-block;
+			margin-right: 0.4rem;
+			width: 1rem;
+			height: 1rem;
+			border-radius: 2rem;
+			background: ${isOpen ? '#37c267' : '#b5325d'};
+		`}
+	></span>
+)
