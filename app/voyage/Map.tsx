38d4934adb696444a6c60e85ec90a1c568893b7c
Mael <mael.thomas@data.gouv.fr>
2023-11-22 23:44:28 +0100

    Ordre

--- app/voyage/Map.tsx
+++ app/voyage/Map.tsx
@@ -593,22 +593,6 @@ out skel qt;
 					<QuickFeatureSearch category={category} searchParams={searchParams} />
 				)}
 
-				{/* 
-
-
-Alternatives : https://github.com/helgastogova/react-stateful-bottom-sheet?ref=hackernoon.com
-https://github.com/helgastogova/react-stateful-bottom-sheet?ref=hackernoon.com
-bof
-
-mieux : https://github.com/plrs9816/slick-bottom-sheet/
-
-https://codesandbox.io/s/framer-motion-bottom-sheet-for-desktop-with-drag-handle-ov8e0o
-
-https://swipable-modal.vercel.app
-
-
-
-			*/}
 				<ModalSwitch
 					{...{
 						isSheetOpen,
--- app/voyage/ModalSwitch.tsx
+++ app/voyage/ModalSwitch.tsx
@@ -20,3 +20,15 @@ export default function ModalSwitch(props) {
 
 	return <ModalSheet {...props} />
 }
+
+/*
+Alternatives : https://github.com/helgastogova/react-stateful-bottom-sheet?ref=hackernoon.com
+https://github.com/helgastogova/react-stateful-bottom-sheet?ref=hackernoon.com
+bof
+
+mieux : https://github.com/plrs9816/slick-bottom-sheet/
+
+https://codesandbox.io/s/framer-motion-bottom-sheet-for-desktop-with-drag-handle-ov8e0o
+
+https://swipable-modal.vercel.app
+*/
