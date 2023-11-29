e18ad64409c8674eb2ca8c1ea9d714f24f22a92e
Mael <mael.thomas@data.gouv.fr>
2023-11-29 22:21:48 +0100

    Titres pour les quicksearch

--- app/voyage/QuickFeatureSearch.tsx
+++ app/voyage/QuickFeatureSearch.tsx
@@ -69,6 +69,7 @@ export default function QuickFeatureSearch({
 								vertical-align: middle;
 							}
 						`}
+						title={category.name}
 					>
 						<Link
 							href={setSearchParams(newSearchParams, true, true)}
