25d35ebd7bfc9898e1e3ce08c3922c5f065ba2f4
Mael <mael.thomas@data.gouv.fr>
2023-12-26 13:02:50 +0100

    Déplacement d'un gros hook, qui dessine les features trouvées par overpass

--- app/voyage/Map.tsx
+++ app/voyage/Map.tsx
@@ -6,26 +6,23 @@ import { createPolygon, createSearchBBox } from './createSearchPolygon'
 import { sortGares } from './gares'
 
 import useSetSearchParams from '@/components/useSetSearchParams'
-import { computeCssVariable } from '@/components/utils/colors'
-import { fromHTML } from '@/components/utils/htmlUtils'
 import { getCategory } from '@/components/voyage/categories'
 import { extractOsmFeature } from '@/components/voyage/fetchPhoton'
 import MapButtons from '@/components/voyage/MapButtons'
 import { goodIconSize } from '@/components/voyage/mapUtils'
 import { centerOfMass } from '@turf/turf'
-import parseOpeningHours from 'opening_hours'
 import useAddMap, { defaultZoom } from './effects/useAddMap'
 import useDrawRoute from './itinerary/useDrawRoute'
 import useItinerary from './itinerary/useItinerary'
 import ModalSwitch from './ModalSwitch'
 import { disambiguateWayRelation, osmRequest } from './osmRequest'
-import { categoryIconUrl } from './QuickFeatureSearch'
 import { styles } from './styles'
 import { MapContainer, MapHeader } from './UI'
 import useHoverOnMapFeatures from './useHoverOnMapFeatures'
 import useTerrainControl from './useTerrainControl'
 import { decodePlace, encodePlace } from './utils'
 import { useZoneImages } from './ZoneImages'
+import useDrawQuickSearchFeatures from './effects/useDrawQuickSearchFeatures'
 
 export const defaultState = {
 	depuis: { inputValue: null, choice: false },
@@ -168,157 +165,7 @@ out skel qt;
 		fetchCategories()
 	}, [category, map])
 
-	useEffect(() => {
-		if (!map || features.length < 1 || !category) return
-
-		const shownFeatures = !showOpenOnly
-			? features
-			: features.filter((f) => {
-					if (!f.tags || f.tags.opening_hours) return false
-					try {
-						const oh = new parseOpeningHours(f.tags.opening_hours, {
-							address: { country_code: 'fr' },
-						})
-						return oh.getState()
-					} catch (e) {
-						return false
-					}
-			  })
-
-		const asyncLoadImage = async () => {
-			const imageUrl = categoryIconUrl(category)
-
-			const imageRequest = await fetch(imageUrl)
-			const imageText = await imageRequest.text()
-
-			// If both the image and svg are found, replace the image with the svg.
-			const img = new Image(40, 40)
-
-			const svg = fromHTML(imageText)
-			const svgSize = svg.getAttribute('width'), // Icons must be square !
-				xyr = svgSize / 2
-			const backgroundDisk = `<circle
-     style="fill:${encodeURIComponent(
-				computeCssVariable('--color')
-			)};fill-rule:evenodd"
-     cx="${xyr}"
-     cy="${xyr}"
-     r="${xyr}" />`
-			const newInner = `${backgroundDisk}<g style="fill:white;" transform="scale(.7)" transform-origin="center" transform-box="fill-box">${svg.innerHTML}</g>`
-			svg.innerHTML = newInner
-			console.log('svg', newInner)
-			console.log(svg.outerHTML)
-
-			img.src = 'data:image/svg+xml;charset=utf-8,' + svg.outerHTML
-
-			img.onload = () => {
-				const imageName = category.name + '-futureco'
-				const mapImage = map.getImage(imageName)
-				if (!mapImage) map.addImage(imageName, img)
-				console.log('OYOYO', category.name, img)
-
-				console.log('features', shownFeatures, features)
-				map.addSource('features-points', {
-					type: 'geojson',
-					data: {
-						type: 'FeatureCollection',
-						features: shownFeatures.map((f) => {
-							const geometry = {
-								type: 'Point',
-								coordinates: [f.lon, f.lat],
-							}
-
-							const tags = f.tags || {}
-							return {
-								type: 'Feature',
-								geometry,
-								properties: {
-									id: f.id,
-									tags,
-									name: tags.name,
-									featureType: f.type,
-								},
-							}
-						}),
-					},
-				})
-				map.addSource('features-ways', {
-					type: 'geojson',
-					data: {
-						type: 'FeatureCollection',
-						features: shownFeatures
-							.filter((f) => f.polygon)
-							.map((f) => {
-								const tags = f.tags || {}
-								return {
-									type: 'Feature',
-									geometry: f.polygon.geometry,
-									properties: {
-										id: f.id,
-										tags,
-										name: tags.name,
-									},
-								}
-							}),
-					},
-				})
-
-				// Add a symbol layer
-				map.addLayer({
-					id: 'features-ways',
-					type: 'fill',
-					source: 'features-ways',
-					layout: {},
-					paint: {
-						'fill-color': computeCssVariable('--lightestColor'),
-						'fill-opacity': 0.6,
-					},
-				})
-				map.addLayer({
-					id: 'features-ways-outlines',
-					type: 'line',
-					source: 'features-ways',
-					layout: {},
-					paint: {
-						'line-color': computeCssVariable('--color'),
-						'line-width': 2,
-					},
-				})
-				map.addLayer({
-					id: 'features-points',
-					type: 'symbol',
-					source: 'features-points',
-					layout: {
-						'icon-image': category.name + '-futureco',
-						'icon-size': 0.6,
-						'text-field': ['get', 'name'],
-						'text-offset': [0, 1.25],
-						'text-anchor': 'top',
-					},
-					paint: {
-						'text-color': '#503f38',
-						'text-halo-blur': 0.5,
-						'text-halo-color': 'white',
-						'text-halo-width': 1,
-					},
-				})
-			}
-		}
-
-		asyncLoadImage()
-
-		return () => {
-			try {
-				map.removeLayer('features-points')
-				map.removeSource('features-points')
-				map.removeLayer('features-ways')
-				map.removeLayer('features-ways-outlines')
-				map.removeSource('features-ways')
-			} catch (e) {
-				console.warn(e)
-			}
-		}
-	}, [features, map, showOpenOnly, category])
+	useDrawQuickSearchFeatures(map, features, showOpenOnly, category)
 
 	useEffect(() => {
 		if (!center || !clickedGare) return
