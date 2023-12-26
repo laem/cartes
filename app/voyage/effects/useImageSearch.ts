9a2dfe4493b122a7d8d7a5f074ee1595cc3b3a65
Mael <mael.thomas@data.gouv.fr>
2023-12-26 15:37:44 +0100

    Pas facile de gérer la MAJ de téléchargment d'images en fct du bbox

--- app/voyage/effects/useImageSearch.ts
+++ app/voyage/effects/useImageSearch.ts
@@ -1,26 +1,38 @@
 import { goodIconSize } from '@/components/voyage/mapUtils'
 import maplibregl from 'maplibre-gl'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
+const serializeBbox = (map) => {
+	if (!map) return null
+
+	const bbox = map.getBounds().toArray()
+	const [[lng2, lat1], [lng1, lat2]] = bbox,
+		bboxString = `${lat2}|${lng2}|${lat1}|${lng1}`
+	return bboxString
+}
 export default function useImageSearch(map, zoom) {
-	const [images, setImages] = useState([])
-	const bbox = map && map.getBounds().toArray()
-	useEffect(() => {
-		if (!bbox || zoom < 17) return
-		const makeRequest = async () => {
-			const [[lng2, lat1], [lng1, lat2]] = bbox
+	const [bboxImages, setBboxImages] = useState({})
 
-			const url = `https://commons.wikimedia.org/w/api.php?action=query&list=geosearch&gsbbox=${lat2}|${lng2}|${lat1}|${lng1}&gsnamespace=6&gslimit=10&format=json&origin=*`
+	const bboxString = serializeBbox(map)
+	const images = useMemo(
+		() => bboxImages[bboxString] || [],
+		[bboxString, bboxImages]
+	)
+	useEffect(() => {
+		console.log('bbox', bboxString)
+		if (!bboxString || zoom < 17) return
+		if (images.length) return
+		const makeRequest = async () => {
+			const url = `https://commons.wikimedia.org/w/api.php?action=query&list=geosearch&gsbbox=${bboxString}&gsnamespace=6&gslimit=10&format=json&origin=*`
 			const request = await fetch(url)
 
 			const json = await request.json()
-			const images = json.query.geosearch
-			if (images.length) setImages((oldImages) => [...oldImages, ...images])
+			const newImages = json.query.geosearch
+			if (newImages.length)
+				setBboxImages((old) => ({ ...old, [bboxString]: newImages }))
 		}
 		makeRequest()
-	}, [zoom, bbox])
-
-	console.log('IMAGES', images)
+	}, [zoom, setBboxImages, bboxString, images])
 
 	useEffect(() => {
 		if (!map) return
@@ -38,7 +50,7 @@ export default function useImageSearch(map, zoom) {
 			const size = goodIconSize(zoom, 1.3) + 'px'
 
 			const img = document.createElement('img')
-			img.src = `https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/${image.title}&width=100`
+			img.src = `https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/${image.title}&width=150`
 			img.style.cssText = `
 			width: ${size};height: ${size};border-radius: ${size};
 			object-fit: cover
