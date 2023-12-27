bd59dbee66a91342377f13150315772536d211bc
Mael <mael.thomas@data.gouv.fr>
2023-12-27 19:06:43 +0100

    Nouveau style train :)

--- app/voyage/styles.ts
+++ app/voyage/styles.ts
@@ -1,5 +1,9 @@
+import railStyle from './railStyle'
+
+const key = process.env.NEXT_PUBLIC_MAPTILER
+
 const maptilerUrl = (styleId) =>
-	`https://api.maptiler.com/maps/${styleId}/style.json?key=${process.env.NEXT_PUBLIC_MAPTILER}`
+	`https://api.maptiler.com/maps/${styleId}/style.json?key=${key}`
 export const styles = {
 	base: {
 		url: maptilerUrl('2f80a9c4-e0dd-437d-ae35-2b6c212f830b'),
@@ -28,5 +32,10 @@ export const styles = {
 		name: 'Plan IGN',
 		image: 'IGN.svg',
 		imageAlt: "Logo de l'IGN",
+	},
+	train: {
+		url: railStyle(key),
+		name: 'Carte des rails',
+		emoji: '🛤️',
 	},
 }
