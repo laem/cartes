88c0bf461c387c37736ae3f2bccaed7cf11538ec
Mael <mael.thomas@data.gouv.fr>
2024-01-01 16:53:45 +0100

    Nouveau logo en test, et on essaie d'autres paramètres manifest pour ff

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,9 +12,10 @@ export default function manifest(): MetadataRoute.Manifest {
 		theme_color: '#2988e6',
 		icons: [
 			{
-				src: 'https://futur.eco/icon.svg',
-				sizes: 'any',
+				src: 'https://futur.eco/voyage.svg',
+				sizes: '48x48 72x72 96x96 128x128 256x256',
 				type: 'image/svg+xml',
+				purpose: 'any',
 			},
 		],
 	}
