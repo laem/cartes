f8c16e025ca1af828d56625925a9767e367cb31a
Mael <mael.thomas@data.gouv.fr>
2024-01-18 11:29:18 +0100

    CSS beaucoup plus propre pour les QuickFeatureSearch :)

--- app/voyage/QuickFeatureSearch.tsx
+++ app/voyage/QuickFeatureSearch.tsx
@@ -2,8 +2,12 @@ import useSetSearchParams from '@/components/useSetSearchParams'
 import { omit } from '@/components/utils/utils'
 import Fuse from 'fuse.js'
 import Link from 'next/link'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import categories from './categories.yaml'
+import {
+	quickSearchButtonStyle,
+	QuickSearchButtonStyle,
+} from './QuickFeatureSearchUI'
 
 export const categoryIconUrl = (category) => {
 	if (!category.icon)
@@ -16,8 +20,6 @@ export const categoryIconUrl = (category) => {
 	return url
 }
 
-const width = '2.2rem'
-
 const fuse = new Fuse(categories, {
 	keys: ['name', 'title', 'query', 'dictionary'],
 	includeScore: true,
@@ -28,6 +30,7 @@ export default function QuickFeatureSearch({
 	searchParams, // dunno why params is not getting updated here, but updates hash though, we need searchParams
 	searchInput,
 }) {
+	const [showMore, setShowMore] = useState(false)
 	const hasLieu = searchParams.lieu
 	const setSearchParams = useSetSearchParams()
 	console.log('hasLieu', hasLieu)
@@ -43,132 +46,85 @@ export default function QuickFeatureSearch({
 		[searchInput, hasLieu]
 	)
 	return (
-		<div
-			css={`
-				margin-top: 0.8rem;
-				overflow: hidden;
-				overflow-x: scroll;
-				white-space: nowrap;
-				height: 3.5rem;
-				scrollbar-width: none;
-				&::-webkit-scrollbar {
-					width: 0px;
-					background: transparent; /* Disable scrollbar Chrome/Safari/Webkit */
-				}
-			`}
-		>
-			<ul
+		<div>
+			<div
 				css={`
-					padding: 0;
-					list-style-type: none;
-					display: flex;
-					align-items: center;
-
-					li {
-						border-radius: ${width};
-
-						margin-right: 0.2rem;
-						img {
-							padding: 0.2rem 0.2rem 0.1rem 0.2rem;
-						}
-						border: 2px solid var(--lighterColor);
+					margin-top: 0.8rem;
+					overflow: hidden;
+					overflow-x: scroll;
+					white-space: nowrap;
+					height: 3.5rem;
+					scrollbar-width: none;
+					&::-webkit-scrollbar {
+						width: 0px;
+						background: transparent; /* Disable scrollbar Chrome/Safari/Webkit */
 					}
-					li a {
-						width: ${width};
-						height: ${width};
-						padding: 0;
-						display: flex;
-						align-items: center;
-						justify-content: center;
-					}
-					li a img {
-						padding: 0;
-						margin: 0;
-						width: 1.2rem;
-						height: 1.2rem;
-					}
+					width: calc(100% - 3rem);
 				`}
 			>
-				<li
-					key="photos"
+				<ul
 					css={`
-						img {
-							width: 1.6rem;
-							height: auto;
-							vertical-align: middle;
-						}
-						background: ${searchParams.photos !== 'oui'
-							? 'white'
-							: 'var(--lighterColor)'};
-
-						${searchParams.photos === 'oui' &&
-						`border-color: var(--darkColor) !important;
-
-						img {
-							filter: invert(23%) sepia(100%) saturate(1940%) hue-rotate(206deg)
-								brightness(89%) contrast(84%);
-						}`}
+						padding: 0;
+						list-style-type: none;
+						display: flex;
+						align-items: center;
 					`}
 				>
-					<Link
-						href={setSearchParams(
-							{
-								...omit(['photos'], searchParams),
-								...(searchParams.photos ? {} : { photos: 'oui' }),
-							},
-							true,
-							true
-						)}
+					<li
+						key="photos"
+						css={`
+							${quickSearchButtonStyle(searchParams.photos === 'oui')}
+						`}
 					>
-						<img src={'/icons/photo.svg'} />
-					</Link>
-				</li>
-				{filteredCategories.map((category) => {
-					const newSearchParams = {
-						...omit(['cat'], searchParams),
-						...(!categorySet || categorySet.name !== category.name
-							? { cat: category.name }
-							: {}),
-					}
-
-					return (
-						<li
-							key={category.name}
-							css={`
-								text-align: center;
-								img {
-									width: 1.6rem;
-									height: auto;
-									vertical-align: middle;
-									filter: invert(16%) sepia(24%) saturate(3004%)
-										hue-rotate(180deg) brightness(89%) contrast(98%);
-								}
-								background: ${!categorySet
-									? 'white'
-									: categorySet.name === category.name
-									? 'var(--lighterColor)'
-									: 'white'};
-
-								${categorySet?.name === category.name &&
-								`border-color: var(--darkColor) !important;
-
-img {filter: invert(23%) sepia(100%) saturate(1940%) hue-rotate(206deg) brightness(89%) contrast(84%);}
-
-								`}
-							`}
-							title={category.title || category.name}
+						<Link
+							href={setSearchParams(
+								{
+									...omit(['photos'], searchParams),
+									...(searchParams.photos ? {} : { photos: 'oui' }),
+								},
+								true,
+								true
+							)}
 						>
-							<Link
-								href={setSearchParams(newSearchParams, true, true)}
-								replace={true}
-								prefetch={false}
+							<img src={'/icons/photo.svg'} />
+						</Link>
+					</li>
+					{filteredCategories.map((category) => {
+						const newSearchParams = {
+							...omit(['cat'], searchParams),
+							...(!categorySet || categorySet.name !== category.name
+								? { cat: category.name }
+								: {}),
+						}
+
+						return (
+							<li
+								key={category.name}
+								css={`
+									${quickSearchButtonStyle(categorySet?.name === category.name)}
+								`}
+								title={category.title || category.name}
 							>
-								<img src={categoryIconUrl(category)} />
-							</Link>
-						</li>
-					)
-				})}
-			</ul>
+								<Link
+									href={setSearchParams(newSearchParams, true, true)}
+									replace={true}
+									prefetch={false}
+								>
+									<img src={categoryIconUrl(category)} />
+								</Link>
+							</li>
+						)
+					})}
+				</ul>
+			</div>
+			<div
+				css={`
+					background: var(--color);
+					color: white;
+				`}
+			>
+				+
+			</div>
 		</div>
 	)
 }
