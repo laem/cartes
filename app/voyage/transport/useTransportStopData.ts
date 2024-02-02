bf55c7125be65d7d9bc30cc22e8ba33ae777372e
Mael <mael.thomas@data.gouv.fr>
2024-02-02 18:34:41 +0100

    Passage à l'URL de prod

--- app/voyage/transport/useTransportStopData.ts
+++ app/voyage/transport/useTransportStopData.ts
@@ -8,8 +8,8 @@ export default function useTransportStopData(osmFeature) {
 		const stopId = findStopId(osmFeature.tags)
 		const doFetch = async () => {
 			const response = await fetch(
-				//'https://gtfs-server.osc-fr1.scalingo.io/stopTimes/' + stopId,
-				'http://localhost:3000/stopTimes/' + stopId,
+				'https://gtfs-server.osc-fr1.scalingo.io/stopTimes/' + stopId,
+				//'http://localhost:3000/stopTimes/' + stopId,
 				{ mode: 'cors' }
 			)
 			const json = await response.json()
