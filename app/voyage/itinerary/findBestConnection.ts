535a52e6a0d23e608549af8bb56cf8b45775bde7
Mael <mael.thomas@data.gouv.fr>
2024-02-19 18:54:08 +0100

    :bug: Introduit par bestConnection

--- app/voyage/itinerary/findBestConnection.ts
+++ app/voyage/itinerary/findBestConnection.ts
@@ -11,13 +11,18 @@ export default function findBestConnection(connections) {
 	 * Very simple algorithm to find a best candidate
 	 * to be highlighted at the top
 	 * */
-	const best = connections
-		.filter((connection) => connection.trips.length === 1)
-		.reduce((memo, next) => {
-			if (memo === null) return next
-			if (connectionDuration(next) < connectionDuration(memo)) return next
-			return memo
-		}, null)
+	const directs = connections.filter(
+		(connection) => connection.trips.length === 1
+	)
+	if (!directs.length) return null
+
+	console.log('prune directs', directs)
+
+	const best = directs.reduce((memo, next) => {
+		if (memo === null) return next
+		if (connectionDuration(next) < connectionDuration(memo)) return next
+		return memo
+	}, null)
 
 	return { best, interval: getBestIntervals(connections, best) }
 }
@@ -26,7 +31,9 @@ export const bestSignature = (connection) => connection.trips[0].id.line_id
 
 export const getBestIntervals = (connections, best) => {
 	const bests = connections.filter(
-		(connection) => bestSignature(connection) === bestSignature(best)
+		(connection) =>
+			connection.trips.length === 1 &&
+			bestSignature(connection) === bestSignature(best)
 	)
 	const departures = bests.map(
 		(connection) => connection.stops[0].departure.time
