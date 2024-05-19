3ed6f26419d551aa09bd7c30ee3e5957a23b9c46
Mael <laem@kont.me>
2024-05-19 14:42:36 +0200

    MAJ des métadonnées

--- app/voyage/layout.tsx
+++ app/voyage/layout.tsx
@@ -5,11 +5,12 @@ import StyledComponentsRegistry from '../../lib/registry'
 import '../globals.css'
 import { ThemeColorsProvider } from '@/components/utils/colors'
 
-const title = `Voyagez, autrement.`
+const title = `Cartes`
 const description1 =
-	"Comment voyager sans voiture ? Comment gérer les derniers kilomètres après l'arrivée à la gare ? Comment se déplacer pendant le weekend ? Où louer une voiture ou un vélo ? On vous guide, pour que le voyage sans voiture personnelle soit un plaisir."
+	'Des cartes libres et gratuites sur le Web pour trouver un commerce, un lieu, visiter une ville, calculer un trajet à pieds, en vélo, en transport en commun, en train.'
 
-export const objectives = ['voyage . trajet voiture . coût trajet par personne']
+//old description to be rewritter when we really cover train+vélo
+//"Comment voyager sans voiture ? Comment gérer les derniers kilomètres après l'arrivée à la gare ? Comment se déplacer pendant le weekend ? Où louer une voiture ou un vélo ? On vous guide, pour que le voyage sans voiture personnelle soit un plaisir."
 
 export async function generateMetadata(
 	{ params, searchParams }: Props,
@@ -23,7 +24,7 @@ export async function generateMetadata(
 		openGraph: {
 			images: [image],
 			type: 'article',
-			publishedTime: '2023-11-01T00:00:00.000Z',
+			publishedTime: '2024-05-10T00:00:00.000Z',
 		},
 	}
 }
