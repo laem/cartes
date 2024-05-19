eb0b72bd2617d34a94b51a6c2b672a628ed3462d
Mael <laem@kont.me>
2024-05-19 14:51:15 +0200

    MAJ de l'intro

--- app/voyage/ContentUI.tsx
+++ app/voyage/ContentUI.tsx
@@ -3,13 +3,13 @@
 import styled from 'styled-components'
 
 export const ExplanationWrapper = styled.div`
-	h2 {
+	h1 {
 		margin-top: 0;
 	}
 	ol {
 		padding-left: 1.5rem;
 	}
-	margin: 0 1rem;
+	margin: 1vw 1rem;
 	a {
 		color: var(--darkestColor);
 	}
