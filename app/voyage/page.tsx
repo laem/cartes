b31589e83a46091c29baaeca852c1173698de7db
Mael <laem@kont.me>
2024-05-20 12:31:22 +0200

    Commentaires pour la génération côté de serveur de ce qu'on peut

--- app/voyage/page.tsx
+++ app/voyage/page.tsx
@@ -1,7 +1,12 @@
 import dynamic from 'next/dynamic'
+// Map is forced as dynamic since it can't be rendered by nextjs server-side.
+// There is almost no interest to do that anyway, except image screenshots
 const Map = dynamic(() => import('./Map'), {
 	ssr: false,
 })
+
+// But lots of content components that are not needing the map can be rendered
+// to provide link previews crucial for sharing place links
 
 const Page = ({ searchParams }) => (
 	<main id="voyage" style={{ height: '100%' }}>
