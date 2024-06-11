78606ab6d4623ad449bd6df7725bc4e5f6574b59
Mael <laem@kont.me>
2024-06-11 22:33:07 +0200

    MAJ framer-motion

--- components/react-modal-sheet/SheetContent.tsx
+++ components/react-modal-sheet/SheetContent.tsx
@@ -1,39 +1,41 @@
-import React, { forwardRef } from 'react';
-import { motion } from 'framer-motion';
+import React, { forwardRef } from 'react'
+import { motion } from 'framer-motion'
 
-import { type SheetDraggableProps } from './types';
-import { useSheetScrollerContext, useSheetContext } from './context';
-import { useDragConstraints } from './hooks';
-import { mergeRefs } from './utils';
-import styles from './styles';
+import { type SheetDraggableProps } from './types'
+import { useSheetScrollerContext, useSheetContext } from './context'
+import { useDragConstraints } from './hooks'
+import { mergeRefs } from './utils'
+import styles from './styles'
 
 const SheetContent = forwardRef<any, SheetDraggableProps>(
-  ({ children, style, disableDrag, className = '', ...rest }, ref) => {
-    const sheetContext = useSheetContext();
-    const sheetScrollerContext = useSheetScrollerContext();
-    const { constraintsRef, onMeasureDragConstraints } = useDragConstraints();
+	({ children, style, disableDrag, className = '', ...rest }, ref) => {
+		const sheetContext = useSheetContext()
+		const sheetScrollerContext = useSheetScrollerContext()
+		const { constraintsRef, onMeasureDragConstraints } = useDragConstraints()
 
-    const dragProps =
-      disableDrag || sheetScrollerContext.disableDrag
-        ? undefined
-        : sheetContext.dragProps;
+		const dragProps =
+			disableDrag || sheetScrollerContext.disableDrag
+				? undefined
+				: sheetContext.dragProps
 
-    return (
-      <motion.div
-        {...rest}
-        ref={mergeRefs([ref, constraintsRef])}
-        className={`react-modal-sheet-content ${className}`}
-        style={{ ...styles.content, ...style }}
-        {...dragProps}
-        dragConstraints={constraintsRef}
-        onMeasureDragConstraints={onMeasureDragConstraints}
-      >
-        {children}
-      </motion.div>
-    );
-  }
-);
+		console.log('yo', dragProps)
 
-SheetContent.displayName = 'SheetContent';
+		return (
+			<motion.div
+				{...rest}
+				ref={mergeRefs([ref, constraintsRef])}
+				className={`react-modal-sheet-content ${className}`}
+				style={{ ...styles.content, ...style }}
+				{...dragProps}
+				dragConstraints={constraintsRef}
+				onMeasureDragConstraints={onMeasureDragConstraints}
+			>
+				{children}
+			</motion.div>
+		)
+	}
+)
 
-export default SheetContent;
+SheetContent.displayName = 'SheetContent'
+
+export default SheetContent
