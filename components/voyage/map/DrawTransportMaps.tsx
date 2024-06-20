edbfd8d50534337688327f177ae0d9924d86bf6f
Mael <laem@kont.me>
2024-06-20 18:47:55 +0200

    Zoom automatique vers la bbox pour faire marcher le sitemap agences

--- components/voyage/map/DrawTransportMaps.tsx
+++ components/voyage/map/DrawTransportMaps.tsx
@@ -1,8 +1,9 @@
 import useDrawTransport from '@/app/voyage/effects/useDrawTransport'
+import { gtfsServerUrl } from '@/app/voyage/serverUrls'
 import { defaultTransitFilter } from '@/app/voyage/transport/TransitFilter'
 import { filterTransportFeatures } from '@/app/voyage/transport/filterTransportFeatures'
 import { sortBy } from '@/components/utils/utils'
-import { useEffect, useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 export default function DrawTransportMaps({
 	map,
@@ -25,6 +26,47 @@ export default function DrawTransportMaps({
 		arret: stop,
 		agence: selectedAgency,
 	} = searchParams
+
+	const [selectedAgencyBbox, setSelectedAgencyBbox] = useState(null)
+
+	useEffect(() => {
+		if (!map) return
+		if (!selectedAgencyBbox) return
+		const bbox = selectedAgencyBbox
+
+		map.fitBounds(
+			[
+				[bbox[0], bbox[1]],
+				[bbox[2], bbox[3]],
+			],
+
+			{
+				padding: { top: 50, bottom: 50, left: 100, right: 100 },
+				duration: 600,
+			}
+		)
+	}, [selectedAgencyBbox, map])
+
+	useEffect(() => {
+		if (!selectedAgency) return
+
+		if (selectedAgencyBbox) return
+		if (transportsData?.find(([agencyId]) => agencyId === selectedAgency))
+			return
+
+		const doFetch = async () => {
+			try {
+				const request = await fetch(
+					gtfsServerUrl + '/agencyBbox/' + selectedAgency
+				)
+				const json = await request.json()
+				setSelectedAgencyBbox(json)
+			} catch (e) {
+				console.error('Error fetching agency bbox')
+			}
+		}
+		doFetch()
+	}, [selectedAgency, transportsData, selectedAgencyBbox])
 
 	const agencyIdsHash = sortBy((id) => id)(
 		transportsData.map(([id]) => id)
