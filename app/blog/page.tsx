75bc8e712da1de4d7a9feb854fec30418dad4bda
Mael <laem@kont.me>
2024-07-09 11:24:54 +0200

    Un peu de texte sur /blog et nouvel article en préparation

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,8 @@
 import { allArticles } from '@/.contentlayer/generated'
 import { compareDesc } from 'date-fns'
 import Link from 'next/link'
-import { dateCool } from './utils'
 import { List } from './UI'
+import { dateCool } from './utils'
 
 const title = `Le blog - Cartes`
 const description =
@@ -19,14 +19,21 @@ const Page = () => {
 	)
 	return (
 		<main>
-			<h1>Le blog</h1>
+			<h1>Le blog de Cartes.app</h1>
+			<p>{description}</p>
+			<p>
+				Pour l'instant, nous sommes dans une phase de construction : l'objectif
+				est de sortir une version 1 en 2024, et ces articles en expliquent
+				l'avancement. L'application reste largement utilisable, mais
+				attendez-vous à quelques bugs.
+			</p>
 			<List>
 				{articles.map(({ url, date, titre }) => (
 					<li key={url}>
 						<div>
 							<Link href={url}>{titre}</Link>
 						</div>
-						<small>{dateCool(date)}</small>
+						<small>publié {dateCool(date)}</small>
 					</li>
 				))}
 			</List>
