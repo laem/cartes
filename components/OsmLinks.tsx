ecc0cf258bfc734105d3849c15376a1f854b69ef
Mael <laem@kont.me>
2024-07-22 17:41:25 +0200

    Revue du design du lien OSM sur les fiches lieu

--- components/OsmLinks.tsx
+++ components/OsmLinks.tsx
@@ -9,33 +9,45 @@ export default function OsmLinks({
 			css={`
 				display: flex;
 				margin-top: 2rem;
+				margin-bottom: 0.2rem;
 				justify-content: center;
 				align-items: center;
 				img {
-					margin: 0 0.6rem;
+					width: 1.6rem;
+					height: auto;
+					margin-right: 0.4rem;
+					vertical-align: middle;
 				}
-				background: var(--lightestColor);
+				background: white;
+				border: 1px solid var(--lightestColor);
 				border-radius: 0.4rem;
-				padding: 0.1rem 0.8rem;
+				padding: 0.3rem 0.8rem;
 				font-size: 80%;
+				a {
+					color: #333;
+					text-decoration: none;
+				}
 			`}
 		>
-			<Image src={osmLogo} width="30" height="30" alt="Logo d'OpenStreetMap" />
-			<div>
-				<a
-					href={`https://openstreetmap.org/${featureType}/${id}`}
-					target="_blank"
-				>
-					voir sur OSM.org
-				</a>
-				&nbsp;-&nbsp;
-				<a
-					href={`https://openstreetmap.org/edit?${featureType}=${id}`}
-					target="_blank"
-				>
-					améliorer
-				</a>
-			</div>
+			<a
+				href={`https://openstreetmap.org/${featureType}/${id}`}
+				target="_blank"
+				title="Voir la fiche OpenStreetMap de ce lieu"
+			>
+				<Image
+					src={osmLogo}
+					width="30"
+					height="30"
+					alt="Logo d'OpenStreetMap"
+				/>
+			</a>
+			<a
+				href={`https://openstreetmap.org/edit?${featureType}=${id}`}
+				target="_blank"
+				title="Ajouter des informations à ce lieu sur l'éditeur OpenStreetMap"
+			>
+				Compléter ce lieu sur OpenStreetMap
+			</a>
 		</div>
 	)
 }
