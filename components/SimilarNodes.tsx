47e28e4cb121a947b34ed2a125d06c0289f95674
Mael <laem@kont.me>
2024-07-22 20:08:41 +0200

    Habillage de la liste des lieux proches

--- components/SimilarNodes.tsx
+++ components/SimilarNodes.tsx
@@ -1,14 +1,28 @@
-import categories from '@/app/categories.yaml'
-import turfDistance from '@turf/distance'
-import useOverpassRequest from '@/app/effects/useOverpassRequest'
-import { roundToTens, roundToThousands, sortBy } from './utils/utils'
-import { humanDuration } from '@/app/itinerary/transit/utils'
 import { computeHumanDistance } from '@/app/RouteRésumé'
+import { buildAllezPart } from '@/app/SetDestination'
+import categories from '@/app/categories.yaml'
+import moreCategories from '@/app/moreCategories.yaml'
+import useOverpassRequest from '@/app/effects/useOverpassRequest'
+import { encodePlace } from '@/app/utils'
+import turfDistance from '@turf/distance'
+import Link from 'next/link'
+import useSetSearchParams from './useSetSearchParams'
+import { capitalise0, sortBy } from './utils/utils'
 
+// This is very scientific haha
+const latDifferenceOfRennes = 0.07,
+	lonDifferenceOfRennes = 0.15,
+	latDiff = latDifferenceOfRennes / 2,
+	lonDiff = lonDifferenceOfRennes / 2
+// 48.07729814876498,-1.7461581764997334,48.148123804291316,-1.5894174840209132
+
+const allCategories = [...categories, ...moreCategories]
 export default function SimilarNodes({ node }) {
 	const { tags } = node
 
-	const category = categories.find(({ query: queryRaw }) => {
+	const setSearchParams = useSetSearchParams()
+
+	const category = allCategories.find(({ query: queryRaw }) => {
 		const query = Array.isArray(queryRaw) ? queryRaw : [queryRaw]
 		return Object.entries(tags).find(([k, v]) =>
 			query.find(
@@ -17,12 +31,6 @@ export default function SimilarNodes({ node }) {
 		)
 	})
 
-	// This is very scientific haha
-	const latDifferenceOfRennes = 0.07,
-		lonDifferenceOfRennes = 0.15,
-		latDiff = latDifferenceOfRennes / 2,
-		lonDiff = lonDifferenceOfRennes / 2
-	// 48.07729814876498,-1.7461581764997334,48.148123804291316,-1.5894174840209132
 	const { lat, lon } = node
 	const bbox = [
 		lat - latDiff / 2,
@@ -31,6 +39,7 @@ export default function SimilarNodes({ node }) {
 		lon + lonDiff / 2,
 	]
 	const [features] = useOverpassRequest(bbox, category)
+	if (!features?.length) return
 
 	const reference = [lon, lat]
 	const featuresWithDistance =
@@ -55,23 +64,76 @@ export default function SimilarNodes({ node }) {
 	 *
 	 * */
 
+	const title = category.title || capitalise0(category.name)
 	return (
-		<ul
+		<section
 			css={`
-				margin-top: 1rem;
-				margin-left: 1rem;
+				margin-top: 2rem;
+				background: white;
+				border: 1px solid var(--lightestColor);
+				border-radius: 0.4rem;
+				padding: 0.3rem 0.8rem;
+				h3 {
+					margin-top: 0.4rem;
+				}
 			`}
 		>
-			{' '}
-			{closestFeatures &&
-				closestFeatures.map((f) => {
-					const humanDistance = computeHumanDistance(f.distance * 1000)
-					return (
-						<li key={f.id}>
-							{f.tags.name} à {humanDistance[0]} {humanDistance[1]}
-						</li>
-					)
-				})}
-		</ul>
+			{closestFeatures && (
+				<>
+					{' '}
+					<h3>{title} proches :</h3>
+					<NodeList
+						nodes={closestFeatures.slice(0, 10)}
+						setSearchParams={setSearchParams}
+					/>
+					<details
+						css={`
+							margin-top: 1rem;
+							margin-bottom: 0.4rem;
+						`}
+					>
+						<summary>Tous les {title} proches</summary>
+						<NodeList
+							nodes={closestFeatures.slice(10)}
+							setSearchParams={setSearchParams}
+						/>
+					</details>
+				</>
+			)}
+		</section>
 	)
 }
+
+const NodeList = ({ nodes, setSearchParams }) => (
+	<ul
+		css={`
+			margin-left: 1.4rem;
+		`}
+	>
+		{nodes.map((f) => {
+			const humanDistance = computeHumanDistance(f.distance * 1000)
+			return (
+				<li key={f.id}>
+					<Link
+						href={setSearchParams(
+							{
+								allez: buildAllezPart(
+									f.tags.name,
+									encodePlace(f.type, f.id),
+									f.lon,
+									f.lat
+								),
+							},
+							true
+						)}
+					>
+						{f.tags.name}
+					</Link>{' '}
+					<small>
+						à {humanDistance[0]} {humanDistance[1]}
+					</small>
+				</li>
+			)
+		})}
+	</ul>
+)
