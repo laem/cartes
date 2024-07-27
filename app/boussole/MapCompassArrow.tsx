ecfa9a7cdf75023e8606a7d3ef7c69d2b50ca8b8
Mael <laem@kont.me>
2024-07-27 15:14:23 +0200

    Plus petit et essai de non inversion

--- app/boussole/MapCompassArrow.tsx
+++ app/boussole/MapCompassArrow.tsx
@@ -12,11 +12,11 @@ export default function MapCompassArrow() {
 				z-index: 20;
 				filter: drop-shadow(0 0 0.75rem white);
 				border-style: solid;
-				border-width: 30px 20px 0 20px;
+				border-width: 12px 6px 0 6px;
 				border-color: red transparent transparent transparent;
 				${compass != null
 					? `
-transform: translate(-50%, -50%) rotate(${-compass}deg) !important;`
+transform: translate(-50%, -50%) rotate(${compass}deg) !important;`
 					: ''}
 			`}
 		></div>
