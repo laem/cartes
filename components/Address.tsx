0d1abd4570972013224fb1c760a6878a8fbac1fd
Mael <laem@kont.me>
2024-07-28 13:20:13 +0200

    Pas de virgule avant un code postal

--- components/Address.tsx
+++ components/Address.tsx
@@ -35,8 +35,12 @@ export const buildAddress = (t: object, noPrefix = false) => {
 		return value || ''
 	}
 
-	const address = `${g('housenumber')} ${g('street')} ${
-		g('postcode') || g('city') || g('state') ? ', ' : ''
+	// This bit of code is ugly, sorry, but had to make it work fast, there's
+	// plenty of things more important than a beautiful address for now
+	const firstPart =
+		g('housenumber') || g('street') ? `${g('housenumber')} ${g('street')}` : ''
+	const address = `${firstPart} ${
+		firstPart && (g('postcode') || g('city') || g('state')) ? ', ' : ''
 	} ${g('postcode')} ${g('city')} ${g('state')}
 `
 	if (address.trim() === '') return null
