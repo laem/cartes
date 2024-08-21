2e3f5a1ba6da94272fcf2e2eea2c48ba3cc8b915
Mael <laem@kont.me>
2024-08-21 22:34:25 +0200

    Affichage des description pour les styles qui en ont

--- app/styles/StyleChooser.tsx
+++ app/styles/StyleChooser.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link'
 import { ModalCloseButton } from '../UI'
 import { styles } from './styles'
 import { useLocalStorage } from 'usehooks-ts'
+import Image from 'next/image'
+import informationIcon from '@/public/information.svg'
 
 const styleList = Object.entries(styles)
 export default function StyleChooser({ style, setStyleChooser, setSnap }) {
@@ -66,49 +68,79 @@ const Styles = ({ style, styleList, setSearchParams }) => {
 				margin-top: 1rem;
 			`}
 		>
-			{styleList.map(([k, { name, imageAlt, title, image: imageProp }]) => {
-				const image = (imageProp || k) + '.png'
+			{styleList.map(
+				([k, { name, imageAlt, title, image: imageProp, description }]) => {
+					const image = (imageProp || k) + '.png'
 
-				return (
-					<li
-						key={k}
-						css={`
-							margin: 0.6rem;
-						`}
-					>
-						<Link
-							href={setSearchParams({ style: k }, true, false)}
-							onClick={() => setLocalStorageStyleKey(k)}
-							title={'Passer au style ' + (title || name)}
+					return (
+						<li
+							key={k}
 							css={`
-								display: flex;
-								flex-direction: column;
-								justify-content: center;
-								align-items: center;
-								text-decoration: none;
-								color: inherit;
-								${style.key === k && `color: var(--color); font-weight: bold`}
+								margin: 0.6rem;
 							`}
 						>
-							<img
-								src={'/styles/' + image}
-								width="50"
-								height="50"
-								alt={imageAlt}
+							<Link
+								href={setSearchParams({ style: k }, true, false)}
+								onClick={() => setLocalStorageStyleKey(k)}
+								title={'Passer au style ' + (title || name)}
 								css={`
-									width: 5.5rem;
-									height: 5.5rem;
-									border-radius: 0.4rem;
-									${style.key === k &&
-									`border: 3px solid var(--color);
+									display: flex;
+									flex-direction: column;
+									justify-content: center;
+									align-items: center;
+									text-decoration: none;
+									color: inherit;
+									${style.key === k && `color: var(--color); font-weight: bold`}
 								`}
+							>
+								<img
+									src={'/styles/' + image}
+									width="50"
+									height="50"
+									alt={imageAlt}
+									css={`
+										width: 5.5rem;
+										height: 5.5rem;
+										border-radius: 0.4rem;
+										${style.key === k &&
+										`border: 3px solid var(--color);
 								`}
-							/>
-							<div>{name}</div>
-						</Link>
-					</li>
-				)
-			})}
+									`}
+								/>
+								<div
+									css={`
+										position: relative;
+										width: 100%;
+										text-align: center;
+										img {
+											position: absolute;
+											right: -0.5rem;
+											top: -0.6rem;
+											width: 1.2rem;
+											height: auto;
+										}
+									`}
+								>
+									{name}
+									{description && (
+										<aside
+											onClick={(e) => {
+												alert(description)
+												e.preventDefault()
+											}}
+										>
+											<Image
+												src={informationIcon}
+												alt="Informations sur le style"
+											/>
+										</aside>
+									)}
+								</div>
+							</Link>
+						</li>
+					)
+				}
+			)}
 		</ul>
 	)
 }
