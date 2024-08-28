33d1a2eda50c561fb1d8fd3be9c512d1819d8788
Mael <laem@kont.me>
2024-08-28 21:40:41 +0200

    Variable viewer mise en mémoire (test)

--- app/Panoramax.tsx
+++ app/Panoramax.tsx
@@ -1,18 +1,22 @@
 import { Viewer } from 'geovisio'
 import { FocusedWrapper } from './FocusedImage'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 export default function Panoramax() {
 	const ref = useRef()
+	const [viewer, setViewer] = useState(null)
 	useEffect(() => {
-		if (!ref || !ref.current) return
+		if (!ref || !ref.current || viewer) return
+		console.log('salut purple')
 		const panoramax = new Viewer(
 			ref.current, // Div ID
 			'https://api.panoramax.xyz/api', // STAC API endpoint
 			{
-				map: true, //selectedPicture: '4ff88765-2500-4328-b888-41d5ae9ed443'
+				map: false,
+				selectedPicture: '4ff88765-2500-4328-b888-41d5ae9ed443',
 			} // Viewer options
 		)
-	}, [ref])
+		setViewer(panoramax)
+	}, [ref, viewer, setViewer])
 	return (
 		<FocusedWrapper>
 			<div
