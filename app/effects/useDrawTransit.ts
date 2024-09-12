89e28b5632127398fdbc01dd2a820a56ff263eb4
Mael <laem@kont.me>
2024-09-12 18:40:06 +0200

    Police bold manquante ; noms de ligne de transport en commun en bold

--- app/effects/useDrawTransit.ts
+++ app/effects/useDrawTransit.ts
@@ -113,7 +113,7 @@ export default function useDrawTransit(map, transit, selectedConnection, date) {
 			source: id,
 			layout: {
 				'symbol-placement': 'line',
-				'text-font': ['Roboto Regular', 'Noto Sans Regular'],
+				'text-font': ['Roboto Bold', 'Noto Sans Bold'],
 				'text-field': '{name}', // part 2 of this is how to do it
 				'text-transform': 'uppercase',
 				'text-size': 16,
