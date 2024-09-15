28fb6a9837e0342871a296e0ad2ff3dfdcbd08f4
Mael <laem@kont.me>
2024-09-15 12:12:48 +0200

    :bug: La vérification de l'existence de WebGl faisait vriller WebGL
    lol, quand on veut prévenir ceux qui n'ont pas de navigateur moderne, et
    qu'on pourrit les navigateurs des 97%...

--- components/isWebglSupported.ts
+++ components/isWebglSupported.ts
@@ -1,20 +1,30 @@
+import { useEffect, useState } from 'react'
+
 export default function isWebglSupported() {
-	if (window.WebGLRenderingContext) {
-		const canvas = document.createElement('canvas')
-		try {
-			// Note that { failIfMajorPerformanceCaveat: true } can be passed as a second argument
-			// to canvas.getContext(), causing the check to fail if hardware rendering is not available. See
-			// https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext
-			// for more details.
-			const context = canvas.getContext('webgl2') || canvas.getContext('webgl')
-			if (context && typeof context.getParameter == 'function') {
-				return true
+	const [isSupported, setIsSupported] = useState(null)
+
+	useEffect(() => {
+		if (isSupported !== null) return
+
+		if (window.WebGLRenderingContext) {
+			const canvas = document.createElement('canvas')
+			try {
+				// Note that { failIfMajorPerformanceCaveat: true } can be passed as a second argument
+				// to canvas.getContext(), causing the check to fail if hardware rendering is not available. See
+				// https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext
+				// for more details.
+				const context =
+					canvas.getContext('webgl2') || canvas.getContext('webgl')
+				if (context && typeof context.getParameter == 'function') {
+					return setIsSupported(true)
+				}
+			} catch (e) {
+				// WebGL is supported, but disabled
 			}
-		} catch (e) {
-			// WebGL is supported, but disabled
+			return setIsSupported(false)
 		}
-		return false
-	}
-	// WebGL not supported
-	return false
+		// WebGL not supported
+		return setIsSupported(false)
+	}, [isSupported, setIsSupported])
+	return isSupported
 }
